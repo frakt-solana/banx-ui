ccffb0164c9298f1c374e73b56bcbc559348cdba
iamsphere <ezekiel.9218@gmail.com>
2023-07-31 17:16:49 +0300

    Add burger menu

---
+++ src/Layout/components/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react'
+
+import classNames from 'classnames'
+
+import { MenuItem } from '../Navbar'
+import { NAVIGATION_LINKS, SECONDARY_NAVIGATION_LINKS } from '../Navbar/constants'
+import { useBurgerMenu } from './hooks'
+
+import styles from './BurgerMenu.module.less'
+
+const BurgerMenu: FC = () => {
+  const { isVisible, toggleVisibility } = useBurgerMenu()
+
+  return (
+    <>
+      <div
+        className={classNames(styles.burgerIcon, { [styles.active]: isVisible })}
+        onClick={toggleVisibility}
+      >
+        <div className={styles.centerIconLine} />
+      </div>
+      <div
+        className={classNames(styles.wrapper, { [styles.hidden]: !isVisible })}
+        onClick={toggleVisibility}
+      >
+        <ul className={styles.navigation}>
+          {[...NAVIGATION_LINKS, ...SECONDARY_NAVIGATION_LINKS].map((item, idx) => (
+            <MenuItem key={`${item?.label}${idx}`} className={styles.link} {...item} />
+          ))}
+        </ul>
+        <div className={styles.community}>
+          <p className={styles.subtitle}>Community</p>
+          {/* <div className={styles.icons}>
+            {COMMUNITY_LINKS.map(({ icon, iconDark, href }, idx) => (
+              <a key={idx} target="_blank" rel="noopener noreferrer" href={href}>
+                {isDark ? iconDark(null) : icon(null)}
+              </a>
+            ))}
+          </div> */}
+        </div>
+        <div className={styles.documentation}>
+          <p className={styles.subtitle}>Documentation</p>
+          {/* <div className={styles.icons}>
+            {DOCUMENTATIONS_LINKS.map(({ icon, iconDark, href }, idx) => (
+              <a key={idx} target="_blank" rel="noopener noreferrer" href={href}>
+                {isDark ? iconDark(null) : icon(null)}
+              </a>
+            ))}
+          </div> */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default BurgerMenu
--- src/Layout/components/Header/Header.tsx
+++ src/Layout/components/Header/Header.tsx
@@ -2,15 +2,21 @@ import { FC } from 'react'
 
 import { Logo, LogoFull } from '@frakt/icons'
 
+import BurgerMenu from '../BurgerMenu'
+
 import styles from './Header.module.less'
 
 export const Header: FC = () => {
   return (
-    <div className={styles.header}>
-      <a href="/" className={styles.logoWrapper}>
-        <LogoFull className={styles.logo} />
-        <Logo className={styles.logoBasic} />
-      </a>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <a href="/" className={styles.logoWrapper}>
+          <LogoFull className={styles.logo} />
+          <Logo className={styles.logoBasic} />
+        </a>
+        <div className={styles.widgetContainer}>widget</div>
+        <BurgerMenu />
+      </div>
     </div>
   )
 }
