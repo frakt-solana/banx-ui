98060d71093191a3661376c8f0795c11709df90f
sablevsky <vladislav1145@gmail.com>
2023-08-15 18:05:37 +0200

    Move TxnError type into transactions folder

--- src/transactions/types.ts
+++ src/transactions/types.ts
@@ -1,3 +1,7 @@
+export interface TxnError extends Error {
+  logs?: Array<string>
+}
+
 export type TransactionParams<T> = T extends (arg: infer Arg) => void
   ? Omit<Arg, 'connection' | 'wallet'>
   : never
