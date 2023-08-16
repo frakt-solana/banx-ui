fa4a31176ac00883704d84b08561cb576c4f438e
sablevsky <vladislav1145@gmail.com>
2023-08-16 13:00:32 +0200

    Create TXN_ERROR_DEFINITIONS constant

---
+++ src/transactions/constants.ts
@@ -0,0 +1,16 @@
+import { NotificationTypes } from '@banx/utils'
+
+import { TxnErrorDefinition, TxnErrorHumanName } from './types'
+
+export const TXN_ERROR_DEFINITIONS: Array<TxnErrorDefinition> = [
+  {
+    humanMessage: TxnErrorHumanName.TRANSACTION_REJECTED,
+    keyphrases: ['Transaction rejected', 'User rejected the request'],
+    type: NotificationTypes.WARNING,
+  },
+  {
+    humanMessage: TxnErrorHumanName.INSUFFICIENT_LAMPORTS,
+    keyphrases: ['insufficient lamports'],
+    type: NotificationTypes.ERROR,
+  },
+]
--- src/transactions/index.ts
+++ src/transactions/index.ts
@@ -1,3 +1,4 @@
 export * from './helpers'
 export * from './types'
 export * from './functions'
+export * from './constants'
