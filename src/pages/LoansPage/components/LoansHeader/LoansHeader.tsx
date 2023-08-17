d9f45be060d11b6dec5f109fd76082a0b4baedba
iamsphere <ezekiel.9218@gmail.com>
2023-08-17 13:23:54 +0300

    Add LoansHeader component

---
+++ src/pages/LoansPage/components/LoansHeader/LoansHeader.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react'
+
+import {
+  AdditionalStat,
+  MainStat,
+  PageHeaderBackdrop,
+  SeparateStatsLine,
+} from '@banx/components/PageHeader'
+import { VALUES_TYPES } from '@banx/components/StatInfo'
+
+const LoansHeader: FC = () => {
+  return (
+    <PageHeaderBackdrop title="Lend">
+      <AdditionalStat label="Loans" value="25" valueType={VALUES_TYPES.STRING} />
+      <AdditionalStat label="Total borrowed" value="120" />
+      <SeparateStatsLine />
+      <MainStat label="Total debt" value="120" />
+    </PageHeaderBackdrop>
+  )
+}
+
+export default LoansHeader
