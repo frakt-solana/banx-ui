26febfc6c31fd3df2de1ec3939691d3aae9d7147
sablevsky <vladislav1145@gmail.com>
2023-08-18 16:42:19 +0200

    Implement new cartState

---
+++ src/pages/BorrowPage/cartState.ts
@@ -0,0 +1,88 @@
+import produce from 'immer'
+import { create } from 'zustand'
+
+import { SimpleOffer, SimpleOffersByMarket } from './types'
+
+interface CartState {
+  offerByMint: Record<string, SimpleOffer>
+  offersByMarket: SimpleOffersByMarket
+
+  addNft: (props: { mint: string; offer: SimpleOffer }) => void
+  removeNft: (props: { mint: string }) => void
+
+  findOfferInCart: (props: { mint: string }) => SimpleOffer | null
+  findBestOffer: (props: { marketPubkey: string }) => SimpleOffer | null
+
+  setCart: (props: { offersByMarket: SimpleOffersByMarket }) => void
+  clearCart: () => void
+}
+
+export const useCartState = create<CartState>((set, get) => ({
+  offerByMint: {},
+  offersByMarket: {},
+
+  findOfferInCart: ({ mint }) => {
+    return get().offerByMint[mint] ?? null
+  },
+
+  findBestOffer: ({ marketPubkey }) => {
+    return get().offersByMarket[marketPubkey]?.at(0) ?? null
+  },
+
+  addNft: ({ mint, offer }) => {
+    if (get().findOfferInCart({ mint })) return
+
+    set(
+      produce((state: CartState) => {
+        state.offerByMint[mint] = offer
+
+        //? Remove offer from offersByMarket
+        state.offersByMarket = {
+          ...state.offersByMarket,
+          [offer.hadoMarket]: state.offersByMarket[offer.hadoMarket].filter(
+            ({ id }) => id !== offer.id,
+          ),
+        }
+      }),
+    )
+  },
+
+  removeNft: ({ mint }) => {
+    set(
+      produce((state: CartState) => {
+        const offerInCart = state.findOfferInCart({ mint })
+
+        if (!offerInCart) return
+
+        delete state.offerByMint[mint]
+
+        //? Put offer from CartNft back to offersByMarket
+        state.offersByMarket = {
+          ...state.offersByMarket,
+          [offerInCart.hadoMarket]: [
+            ...state.offersByMarket[offerInCart.hadoMarket],
+            offerInCart,
+          ].sort((a, b) => {
+            return b.loanValue - a.loanValue
+          }),
+        }
+      }),
+    )
+  },
+
+  setCart: ({ offersByMarket }) => {
+    set(
+      produce((state: CartState) => {
+        state.offerByMint = {}
+        state.offersByMarket = offersByMarket
+      }),
+    )
+  },
+  clearCart: () =>
+    set(
+      produce((state: CartState) => {
+        state.offerByMint = {}
+        state.offersByMarket = {}
+      }),
+    ),
+}))
