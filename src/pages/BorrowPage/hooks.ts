80d8501b4ef7d148ca5db7186d5ecf9686a55c78
sablevsky <vladislav1145@gmail.com>
2023-08-18 16:42:41 +0200

    Add useBorrowNftsAndSimpleOffers hook

---
+++ src/pages/BorrowPage/hooks.ts
@@ -0,0 +1,81 @@
+import { useEffect, useMemo } from 'react'
+
+import { useWallet } from '@solana/wallet-adapter-react'
+import { useQuery } from '@tanstack/react-query'
+import { isEmpty, uniqueId } from 'lodash'
+
+import { Offer, fetchBorrowNftsAndOffers } from '@banx/api/core'
+
+import { useCartState } from './cartState'
+import { SimpleOffer } from './types'
+
+export const useBorrowNftsAndSimpleOffers = () => {
+  const { setCart } = useCartState()
+
+  const { publicKey: walletPublicKey } = useWallet()
+
+  const { data, isLoading } = useQuery(
+    ['walletBorrowNfts', walletPublicKey?.toBase58()],
+    () => fetchBorrowNftsAndOffers({ walletPubkey: walletPublicKey?.toBase58() || '' }),
+    {
+      enabled: !!walletPublicKey,
+      staleTime: 5 * 1000,
+      refetchOnWindowFocus: false,
+    },
+  )
+
+  const offers = useMemo(() => {
+    return Object.fromEntries(
+      Object.entries(data?.offers || {}).map(([marketPubkey, offers]) => {
+        const simpleOffers = offers
+          .map(spreadToSimpleOffers)
+          .flat()
+          .sort((a, b) => {
+            return b.loanValue - a.loanValue
+          })
+        return [marketPubkey, simpleOffers]
+      }),
+    )
+  }, [data])
+
+  //? Set offers in cartState
+  useEffect(() => {
+    if (!isEmpty(offers)) {
+      setCart({ offersByMarket: offers })
+    }
+  }, [setCart, offers])
+
+  return {
+    nfts: data?.nfts || [],
+    isLoading,
+  }
+}
+
+const spreadToSimpleOffers = (offer: Offer): SimpleOffer[] => {
+  const { fundsSolOrTokenBalance, currentSpotPrice } = offer
+
+  const fullOffersAmount = Math.floor(fundsSolOrTokenBalance / currentSpotPrice)
+
+  const offers = Array(fullOffersAmount)
+    .fill(currentSpotPrice)
+    .map((loanValue) => ({
+      id: uniqueId(),
+      loanValue,
+      hadoMarket: offer.hadoMarket,
+      publicKey: offer.publicKey,
+    }))
+
+  const decimalLoanValue = fundsSolOrTokenBalance - currentSpotPrice * fullOffersAmount
+
+  //? Add not full offer
+  if (decimalLoanValue && decimalLoanValue > 0) {
+    offers.push({
+      id: uniqueId(),
+      loanValue: decimalLoanValue,
+      hadoMarket: offer.hadoMarket,
+      publicKey: offer.publicKey,
+    })
+  }
+
+  return offers
+}
