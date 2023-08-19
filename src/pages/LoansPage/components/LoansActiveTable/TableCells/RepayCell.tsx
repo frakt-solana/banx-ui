db85bfaef4473d816c40c6b040b905a5c5ad6b51
iamsphere <ezekiel.9218@gmail.com>
2023-08-19 20:47:08 +0300

    Add signAndSendAllTransactions, implement bulk loan repay

--- src/pages/LoansPage/components/LoansActiveTable/TableCells/RepayCell.tsx
+++ src/pages/LoansPage/components/LoansActiveTable/TableCells/RepayCell.tsx
@@ -1,17 +1,10 @@
 import { FC } from 'react'
 
-import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-
 import { Button } from '@banx/components/Buttons'
 
 import { Loan } from '@banx/api/core'
-import { useWalletLoans } from '@banx/pages/LoansPage/hooks'
-import {
-  MakeTransactionFn,
-  TransactionParams,
-  buildAndExecuteTransaction,
-} from '@banx/transactions'
-import { MakeRepayLoanTransaction, makeRepayLoanTransaction } from '@banx/transactions/loans'
+
+import { useLoansTransactions } from '../../LoansActiveTab/hooks'
 
 interface RepayCellProps {
   loan: Loan
@@ -19,46 +12,17 @@ interface RepayCellProps {
 }
 
 export const RepayCell: FC<RepayCellProps> = ({ loan, isCardView }) => {
-  const onRepayLoan = useRepayLoan(loan)
+  const { repayLoan } = useLoansTransactions()
 
   return (
     <Button
       size={isCardView ? 'large' : 'small'}
       onClick={(event) => {
-        onRepayLoan()
+        repayLoan(loan)
         event.stopPropagation()
       }}
     >
       Repay
     </Button>
   )
-}
-
-const useRepayLoan = (loan: Loan) => {
-  const wallet = useWallet()
-  const { connection } = useConnection()
-
-  const { hideLoan } = useWalletLoans()
-
-  const executeLoanTransaction = async <T extends object>(props: {
-    makeTransactionFn: MakeTransactionFn<TransactionParams<T>>
-    transactionParams: TransactionParams<T>
-    onSuccess: () => void
-  }) => {
-    await buildAndExecuteTransaction({
-      wallet,
-      connection,
-      ...props,
-    })
-  }
-
-  const repayLoan = async () => {
-    await executeLoanTransaction<MakeRepayLoanTransaction>({
-      makeTransactionFn: makeRepayLoanTransaction,
-      transactionParams: { loan },
-      onSuccess: () => hideLoan(loan.publicKey),
-    })
-  }
-
-  return repayLoan
 }
