4397a1a04dd50f7fb70be3ce7c7f81f0e6a649cf
sablevsky <vladislav1145@gmail.com>
2023-08-24 15:15:05 +0200

    Implement useOptimisticLoans store

---
+++ src/store/optimistics/useLoansOptimistic.ts
@@ -0,0 +1,40 @@
+import produce from 'immer'
+import { uniqBy } from 'lodash'
+import { create } from 'zustand'
+
+import { Loan } from '@banx/api/core'
+
+interface LoansOptimisticStore {
+  loans: Loan[]
+  find: (publicKey: string) => Loan | undefined
+  add: (...loans: Loan[]) => void
+  remove: (...publicKeys: string[]) => void
+  update: (...loans: Loan[]) => void
+}
+
+export const useOptimisticLoans = create<LoansOptimisticStore>((set, get) => ({
+  loans: [],
+  find: (publicKey) => {
+    return get().loans.find((l) => l.publicKey === publicKey)
+  },
+  add: (...loans) => {
+    set(
+      produce((state: LoansOptimisticStore) => {
+        state.loans = uniqBy([...state.loans, ...loans], ({ publicKey }) => publicKey)
+      }),
+    )
+  },
+  remove: (...publicKeys) => {
+    set(
+      produce((state: LoansOptimisticStore) => {
+        state.loans.filter(({ publicKey }) => !publicKeys.includes(publicKey))
+      }),
+    )
+  },
+  update: (...loans) => {
+    const { add, remove } = get()
+    const publicKeys = loans.map(({ publicKey }) => publicKey)
+    remove(...publicKeys)
+    add(...loans)
+  },
+}))
