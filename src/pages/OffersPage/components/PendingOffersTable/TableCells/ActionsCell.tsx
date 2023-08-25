11d1f6021c13a510194b6e1a61b9dd499e4319d7
iamsphere <ezekiel.9218@gmail.com>
2023-08-25 11:07:04 +0300

    fix eslint error

--- src/pages/OffersPage/components/PendingOffersTable/TableCells/ActionsCell.tsx
+++ src/pages/OffersPage/components/PendingOffersTable/TableCells/ActionsCell.tsx
@@ -1,12 +1,14 @@
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 
 import { Button } from '@banx/components/Buttons'
 
+import { Offer } from '@banx/api/core'
 import { TxnExecutor } from '@banx/transactions/TxnExecutor'
 import { makeRemoveOfferAction } from '@banx/transactions/bonds'
 
+import { useUserOffers } from '../../PendingOffersTab/hooks'
 import { TableUserOfferData } from '../helpers'
 
 import styles from '../PendingOffersTable.module.less'
@@ -38,19 +40,17 @@ export const ActionsCell: FC<ActionsCellProps> = ({ offer }) => {
 const useActionsCell = (offer: TableUserOfferData) => {
   const wallet = useWallet()
   const { connection } = useConnection()
+  const { offers } = useUserOffers()
 
   const offerPubkey = offer.publicKey
 
-  // const optimisticOffer = useMemo(() => {
-  //   return offers.find((offer) => offer.publicKey === offerPubkey)
-  // }, [offers, offerPubkey])
+  const optimisticOffer = useMemo(() => {
+    return offers.find((offer) => offer.publicKey === offerPubkey)
+  }, [offers, offerPubkey])
 
   const removeOffer = () => {
     new TxnExecutor(makeRemoveOfferAction, { wallet, connection })
-      // .addTxnParam({ offerPubkey: offer.publicKey, optimisticOffer: optimisticOffer as any })
-      // .on('pfSuccess', () => {
-      //   updateOrAddOffer(optimisticOffer as any)
-      // })
+      .addTxnParam({ offerPubkey: offer.publicKey, optimisticOffer: optimisticOffer as Offer })
       .execute()
   }
 
