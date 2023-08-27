2fac458976c4647d033e46b7e9d378a975e5fa4c
sablevsky <vladislav1145@gmail.com>
2023-08-27 14:37:06 +0200

    Add custom zeroPlaceholder prop into createTableValues functions

--- src/components/TableComponents/CommonCells.tsx
+++ src/components/TableComponents/CommonCells.tsx
@@ -2,27 +2,32 @@ import moment from 'moment'
 
 import styles from './TableCells.module.less'
 
-const formatDisplayValue = (initialValue: number, formattedValue: string, unit: string) => {
-  return initialValue ? `${formattedValue}${unit}` : '--'
+const formatDisplayValue = (
+  initialValue: number,
+  formattedValue: string,
+  unit: string,
+  zeroPlaceholder = '--',
+) => {
+  return initialValue ? `${formattedValue}${unit}` : zeroPlaceholder
 }
 
-export const createSolValueJSX = (initialValue = 0, divider = 1) => {
+export const createSolValueJSX = (initialValue = 0, divider = 1, zeroPlaceholder = '--') => {
   const formattedValue = (initialValue / divider).toFixed(2)
-  const displayValue = formatDisplayValue(initialValue, formattedValue, '◎')
+  const displayValue = formatDisplayValue(initialValue, formattedValue, '◎', zeroPlaceholder)
 
   return <span className={styles.value}>{displayValue}</span>
 }
 
-export const createPercentValueJSX = (initialValue = 0) => {
+export const createPercentValueJSX = (initialValue = 0, zeroPlaceholder = '--') => {
   const formattedValue = initialValue.toFixed(0)
-  const displayValue = formatDisplayValue(initialValue, formattedValue, '%')
+  const displayValue = formatDisplayValue(initialValue, formattedValue, '%', zeroPlaceholder)
 
   return <span className={styles.value}>{displayValue}</span>
 }
 
-export const createTimeValueJSX = (initialValue: number) => {
+export const createTimeValueJSX = (initialValue: number, zeroPlaceholder = '--') => {
   const formattedValue = moment.unix(initialValue).fromNow(false)
-  const displayValue = formatDisplayValue(initialValue, formattedValue, '')
+  const displayValue = formatDisplayValue(initialValue, formattedValue, '', zeroPlaceholder)
 
   return <span className={styles.value}>{displayValue}</span>
 }
