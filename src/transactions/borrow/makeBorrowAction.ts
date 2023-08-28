00819fab1afd7299e46d2b943439eb43a7a1c7a1
Vedamir Efanov <vedamire@protonmail.com>
2023-08-28 22:50:22 +0400

    added borrow and repay of staked banx

--- src/transactions/borrow/makeBorrowAction.ts
+++ src/transactions/borrow/makeBorrowAction.ts
@@ -1,6 +1,6 @@
 import { web3 } from 'fbonds-core'
 import { LOOKUP_TABLE } from 'fbonds-core/lib/fbond-protocol/constants'
-import { borrowPerpetual } from 'fbonds-core/lib/fbond-protocol/functions/perpetual'
+import { borrowPerpetual, borrowStakedBanxPerpetual } from 'fbonds-core/lib/fbond-protocol/functions/perpetual'
 import { BondOfferV2 } from 'fbonds-core/lib/fbond-protocol/types'
 
 import { BorrowNft, Loan, Offer } from '@banx/api/core'
@@ -19,12 +19,39 @@ export type MakeBorrowActionResult = Loan[]
 
 export type MakeBorrowAction = MakeActionFn<MakeBorrowActionParams, MakeBorrowActionResult>
 
-export const LOANS_PER_TXN = 2
+export const LOANS_PER_TXN = 1
 
 export const makeBorrowAction: MakeBorrowAction = async (ixnParams, { connection, wallet }) => {
   if (ixnParams.length > LOANS_PER_TXN)
     throw new Error(`Maximum borrow per txn is ${LOANS_PER_TXN}`)
-  const { instructions, signers, optimisticResults } = await borrowPerpetual({
+
+
+  const { instructions, signers, optimisticResults } = ixnParams[0].nft.loan.banxStake ? await borrowStakedBanxPerpetual({
+    programId: new web3.PublicKey(BONDS.PROGRAM_PUBKEY),
+    addComputeUnits: true,
+
+    accounts: {
+      userPubkey: wallet.publicKey as web3.PublicKey,
+      protocolFeeReceiver: new web3.PublicKey(BONDS.ADMIN_PUBKEY),
+    },
+    args: {
+      perpetualBorrowParamsAndAccounts: ixnParams.map(({ nft, offer, loanValue }) => ({
+        amountOfSolToGet: loanValue,
+        minAmountToGet: loanValue,
+        tokenMint: new web3.PublicKey(nft.mint),
+        bondOfferV2: new web3.PublicKey(offer.publicKey),
+        hadoMarket: new web3.PublicKey(offer.hadoMarket),
+        banxStake: new web3.PublicKey(nft.loan.banxStake as any),
+        optimistic: {
+          fraktMarket: nft.loan.fraktMarket,
+          minMarketFee: nft.loan.marketApr,
+          bondOffer: offer as BondOfferV2,
+        },
+      })),
+    },
+    connection,
+    sendTxn: sendTxnPlaceHolder,
+  }) : await borrowPerpetual({
     programId: new web3.PublicKey(BONDS.PROGRAM_PUBKEY),
     addComputeUnits: true,
 
@@ -63,4 +90,5 @@ export const makeBorrowAction: MakeBorrowAction = async (ixnParams, { connection
     additionalResult: loans,
     lookupTables: [new web3.PublicKey(LOOKUP_TABLE)],
   }
+
 }
