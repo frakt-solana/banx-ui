77e6880e8fbfce50c67014de5f824deb66fa74ae
iamsphere <ezekiel.9218@gmail.com>
2023-08-29 14:22:33 +0300

    Refactor APRIncreaseCell, fix timer, add interval

--- src/pages/RefinancePage/components/RefinanceTable/TableCells/APRIncreaseCell.tsx
+++ src/pages/RefinancePage/components/RefinanceTable/TableCells/APRIncreaseCell.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 import moment from 'moment'
 
@@ -15,13 +15,33 @@ interface APRIncreaseCellProps {
 export const APRIncreaseCell: FC<APRIncreaseCellProps> = ({ loan }) => {
   const { refinanceAuctionStartedAt } = loan.fraktBond
 
-  const currentTime = moment()
   const auctionStartTime = moment.unix(refinanceAuctionStartedAt)
+
+  const [currentTime, setCurrentTime] = useState(moment())
+  const [nextRoundStartTime, setNextRoundStartTime] = useState(
+    auctionStartTime.add(1, 'hours').unix(),
+  )
+
+  const updateCurrentTime = () => {
+    setCurrentTime(moment())
+  }
+
+  useEffect(() => {
+    const interval = setInterval(updateCurrentTime, 1000) // Update current time every second
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
   const secondsSinceStart = currentTime.diff(auctionStartTime, 'seconds')
   const elapsedHours = Math.floor(secondsSinceStart / SECONDS_PER_HOUR)
 
-  const startTimeForNextRound = auctionStartTime.add(elapsedHours + 1, 'hours').unix()
+  const shouldRestartTimer = currentTime >= moment.unix(nextRoundStartTime)
 
-  return <Timer expiredAt={startTimeForNextRound} detailedTimeFormat />
+  if (shouldRestartTimer) {
+    setNextRoundStartTime(auctionStartTime.add(elapsedHours + 1, 'hours').unix())
+  }
+
+  return <Timer expiredAt={nextRoundStartTime} detailedTimeFormat />
 }
