850fff9556f327018c4a50fd0a8062c5895e1328
iamsphere <ezekiel.9218@gmail.com>
2023-09-03 19:22:59 +0300

    Display real data for borrower activity

--- src/api/stats/types.ts
+++ src/api/stats/types.ts
@@ -1,12 +1,5 @@
 import { z } from 'zod'
 
-import { PaginationMeta } from '@banx/types'
-
-export interface UserOffersStatsResponse {
-  data: UserOffersStats
-  meta: PaginationMeta
-}
-
 export const UserOffersStatsSchema = z.object({
   loansVolume: z.number(),
   offersVolume: z.number(),
@@ -19,3 +12,21 @@ export const UserOffersStatsSchema = z.object({
 })
 
 export type UserOffersStats = z.infer<typeof UserOffersStatsSchema>
+
+export interface UserOffersStatsResponse {
+  data: UserOffersStats
+}
+
+export const UserLoansStatsSchema = z.object({
+  loans: z.number(),
+  totalBorrowed: z.number(),
+  totalLoans: z.number(),
+  totalDebt: z.number(),
+  totalRepaid: z.number(),
+})
+
+export type UserLoansStats = z.infer<typeof UserLoansStatsSchema>
+
+export interface UserLoansStatsResponse {
+  data: UserLoansStats
+}
