b0a187c4e35d5e4fc038e4aeb6c7e100d23745c9
iamsphere <ezekiel.9218@gmail.com>
2023-09-04 12:10:23 +0300

    fix types

--- src/api/stats/requests.ts
+++ src/api/stats/requests.ts
@@ -2,11 +2,15 @@ import axios from 'axios'
 
 import { BACKEND_BASE_URL } from '@banx/constants'
 
-import { UserLoansStatsResponse, UserOffersStatsResponse, UserOffersStatsSchema } from './types'
+import {
+  UserLoansStats,
+  UserLoansStatsResponse,
+  UserOffersStats,
+  UserOffersStatsResponse,
+  UserOffersStatsSchema,
+} from './types'
 
-type FetchUserOffersStats = (
-  walletPubkey: string,
-) => Promise<UserOffersStatsResponse['data'] | null>
+type FetchUserOffersStats = (walletPubkey: string) => Promise<UserOffersStats | null>
 
 export const fetchUserOffersStats: FetchUserOffersStats = async (walletPubkey) => {
   try {
@@ -27,7 +31,7 @@ export const fetchUserOffersStats: FetchUserOffersStats = async (walletPubkey) =
   }
 }
 
-type FetchUserLoansStats = (walletPubkey: string) => Promise<UserLoansStatsResponse['data'] | null>
+type FetchUserLoansStats = (walletPubkey: string) => Promise<UserLoansStats | null>
 
 export const fetchUserLoansStats: FetchUserLoansStats = async (walletPubkey) => {
   try {
