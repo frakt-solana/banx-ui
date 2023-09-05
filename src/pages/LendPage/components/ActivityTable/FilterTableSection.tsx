d9b260dd61bcd5dffd29fb7c2dcb30a37dddaa62
iamsphere <ezekiel.9218@gmail.com>
2023-09-05 13:40:47 +0300

    Add "Mine" Toggle for lender activity

--- src/pages/LendPage/components/ActivityTable/FilterTableSection.tsx
+++ src/pages/LendPage/components/ActivityTable/FilterTableSection.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import { RBOption, RadioButton } from '@banx/components/RadioButton'
+import { Toggle } from '@banx/components/Toggle'
 
 import { RADIO_BUTTONS_OPTIONS } from './constants'
 
@@ -8,6 +9,7 @@ import styles from './ActivityTable.module.less'
 
 export const FilterTableSection = () => {
   const [selectedOption, setSelectedOption] = useState(RADIO_BUTTONS_OPTIONS[0])
+  const [checked, setChecked] = useState(false)
 
   const handleOptionChange = (newOption: RBOption) => {
     setSelectedOption(newOption)
@@ -20,6 +22,7 @@ export const FilterTableSection = () => {
         currentOption={selectedOption}
         onOptionChange={handleOptionChange}
       />
+      <Toggle label="Mine" checked={checked} onChange={() => setChecked(!checked)} />
     </div>
   )
 }
--- src/pages/LendPage/components/ActivityTable/constants.ts
+++ src/pages/LendPage/components/ActivityTable/constants.ts
@@ -16,4 +16,3 @@ export const RADIO_BUTTONS_OPTIONS = [
     value: 'liquidated',
   },
 ]
-
