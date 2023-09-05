d2c56acbc076ed8d5296d1798986c7478939d35d
iamsphere <ezekiel.9218@gmail.com>
2023-09-05 22:49:45 +0300

    Add useFakeInfinityScroll to LendPage

--- src/pages/LendPage/components/LendPageContent/LendPageContent.tsx
+++ src/pages/LendPage/components/LendPageContent/LendPageContent.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import { isEmpty } from 'lodash'
 
+import { useFakeInfinityScroll } from '@banx/components/InfinityScroll'
 import { Loader } from '@banx/components/Loader'
 
 import { useVisibleMarketURLControl } from '@banx/store/useVisibleMarkets'
@@ -16,17 +17,22 @@ const LendPageContent = () => {
   const { marketsPreview, isLoading, searchSelectParams, sortParams, showEmptyList } =
     useLendPageContent()
 
+  const { data: markets, fetchMoreTrigger } = useFakeInfinityScroll({ rawData: marketsPreview })
+
   return (
     <div className={classNames(styles.content, { [styles.selected]: !!visibleCards?.length })}>
       <FilterSection searchSelectParams={searchSelectParams} sortParams={sortParams} />
       {isLoading && isEmpty(marketsPreview) ? (
         <Loader />
       ) : (
-        <MarketsList
-          markets={marketsPreview}
-          visibleCards={visibleCards}
-          toggleVisibleCard={toggleVisibleCard}
-        />
+        <>
+          <MarketsList
+            markets={markets}
+            visibleCards={visibleCards}
+            toggleVisibleCard={toggleVisibleCard}
+          />
+          <div ref={fetchMoreTrigger} />
+        </>
       )}
       {showEmptyList && <EmptyList />}
     </div>
