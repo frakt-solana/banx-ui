36bbcfe9b2a6afd74de0a5e8ba1f6d8383b1c97e
Vladislav <60342317+iamsphere@users.noreply.github.com>
2023-09-06 14:52:25 +0300

    Add fake infinite scroll for Tables on all pages (#71)
    * Add InfinityScroll component and hooks
    
    * Modify useFakeInfinityScroll
    
    * Add useFakeInfinityScroll to BorrowTable
    
    * Add useFakeInfinityScroll to LoansPage
    
    * Add useFakeInfinityScroll to LendPage
    
    * Add useFakeInfinityScroll to RefinancePage
    
    * Add useFakeInfinityScroll to OffersPage
    
    * Refactor InfinityScroll component
    
    * Move useFakeInfinityScroll to global hooks, remove react-infinite-scroll-component library

---
+++ src/hooks/useIntersection.ts
@@ -0,0 +1,36 @@
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from 'react'
+
+type UseIntersection = (props?: { isSingle?: boolean }) => {
+  ref: Dispatch<SetStateAction<Element | null>>
+  inView: boolean
+}
+
+export const useIntersection: UseIntersection = (props) => {
+  const isSingle = !!props?.isSingle
+
+  const [entry, setEntry] = useState<IntersectionObserverEntry>()
+  const [ref, setRef] = useState<Element | null>(null)
+
+  const frozen = useMemo(() => entry?.isIntersecting && isSingle, [isSingle, entry])
+
+  const updateEntry = ([entry]: IntersectionObserverEntry[]): void => {
+    setEntry(entry)
+  }
+
+  useEffect(() => {
+    const hasIOSupport = !!window.IntersectionObserver
+
+    if (!hasIOSupport || frozen || !ref) return
+
+    const observer = new IntersectionObserver(updateEntry)
+
+    observer.observe(ref)
+
+    return () => observer.disconnect()
+  }, [ref, isSingle, frozen])
+
+  return {
+    ref: setRef,
+    inView: !!entry?.isIntersecting,
+  }
+}
