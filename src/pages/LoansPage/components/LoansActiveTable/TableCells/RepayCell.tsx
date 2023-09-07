085bc16e32e086bfac595089eac61c3acaea1e78
iamsphere <ezekiel.9218@gmail.com>
2023-09-07 13:40:28 +0300

    Add calculating repay, remaining values

--- src/pages/LoansPage/components/LoansActiveTable/TableCells/RepayCell.tsx
+++ src/pages/LoansPage/components/LoansActiveTable/TableCells/RepayCell.tsx
@@ -2,10 +2,13 @@ import { FC, useState } from 'react'
 
 import { Button } from '@banx/components/Buttons'
 import { Slider } from '@banx/components/Slider'
+import { StatInfo } from '@banx/components/StatInfo'
+import { createSolValueJSX } from '@banx/components/TableComponents'
 import { Modal } from '@banx/components/modals/BaseModal'
 
 import { Loan } from '@banx/api/core'
 import { useModal } from '@banx/store'
+import { calculateLoanRepayValue } from '@banx/utils'
 
 import { useLoansTransactions } from '../hooks'
 
@@ -45,21 +48,50 @@ interface RepayModalProps {
 const RepayModal: FC<RepayModalProps> = ({ loan }) => {
   const { close } = useModal()
 
-  const { repayLoan } = useLoansTransactions()
+  const { repayLoan, repayPartialLoan } = useLoansTransactions()
 
-  const onSubmit = () => {}
+  const initialRepayValue = calculateLoanRepayValue(loan)
 
-  const [partialPercent, setPartialPercent] = useState(100)
+  const [partialPercent, setPartialPercent] = useState<number>(100)
+  const [paybackValue, setPaybackValue] = useState<number>(initialRepayValue)
 
-  const onPartialPercentChange = (nextValue: number) => {
-    setPartialPercent(nextValue)
+  const onPartialPercentChange = (percentValue: number) => {
+    setPartialPercent(percentValue)
+    setPaybackValue((initialRepayValue * percentValue) / 100)
+  }
+
+  const remainingValue = initialRepayValue - paybackValue
+
+  const onSubmit = async () => {
+    try {
+      if (partialPercent === 100) {
+        await repayLoan(loan)
+      } else {
+        await repayPartialLoan(loan, partialPercent * 100)
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      close()
+    }
   }
 
   return (
     <Modal open onCancel={close}>
+      <StatInfo
+        flexType="row"
+        label="Debt:"
+        value={initialRepayValue}
+        divider={1e9}
+        classNamesProps={{ container: styles.mainRepayInfo }}
+      />
       <Slider value={partialPercent} onChange={onPartialPercentChange} />
-      <Button className={styles.repayButton} onClick={onSubmit}>
-        Repay
+      <div className={styles.additionalRepayInfo}>
+        <StatInfo flexType="row" label="Repay value" value={paybackValue} divider={1e9} />
+        <StatInfo flexType="row" label="Remaining debt" value={remainingValue} divider={1e9} />
+      </div>
+      <Button className={styles.repayButton} onClick={onSubmit} disabled={!partialPercent}>
+        Repay {createSolValueJSX(paybackValue, 1e9, '0◎')}
       </Button>
     </Modal>
   )
