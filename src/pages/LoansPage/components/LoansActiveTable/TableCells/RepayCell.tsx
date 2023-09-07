443b8a9296bebb9b918ea2d30fc885485395e889
iamsphere <ezekiel.9218@gmail.com>
2023-09-07 12:25:05 +0300

    Add RepayModal

--- src/pages/LoansPage/components/LoansActiveTable/TableCells/RepayCell.tsx
+++ src/pages/LoansPage/components/LoansActiveTable/TableCells/RepayCell.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react'
 
 import { Button } from '@banx/components/Buttons'
+import { Modal } from '@banx/components/modals/BaseModal'
 
 import { Loan } from '@banx/api/core'
+import { useModal } from '@banx/store'
 
 import { useLoansTransactions } from '../hooks'
 
@@ -12,17 +14,41 @@ interface RepayCellProps {
 }
 
 export const RepayCell: FC<RepayCellProps> = ({ loan, isCardView }) => {
-  const { repayLoan } = useLoansTransactions()
+  const { open } = useModal()
+
+  const openModal = () => {
+    open(RepayModal, { loan })
+  }
 
   return (
-    <Button
-      size={isCardView ? 'large' : 'small'}
-      onClick={(event) => {
-        repayLoan(loan)
-        event.stopPropagation()
-      }}
-    >
-      Repay
-    </Button>
+    <>
+      <Button
+        size={isCardView ? 'large' : 'small'}
+        onClick={(event) => {
+          openModal()
+          event.stopPropagation()
+        }}
+      >
+        Repay
+      </Button>
+    </>
+  )
+}
+
+interface RepayModalProps {
+  loan: Loan
+}
+
+const RepayModal: FC<RepayModalProps> = ({ loan }) => {
+  const { close } = useModal()
+
+  const { repayLoan } = useLoansTransactions()
+
+  const onSubmit = () => {}
+
+  return (
+    <Modal open onCancel={close}>
+      <Button onClick={onSubmit}>Repay</Button>
+    </Modal>
   )
 }
