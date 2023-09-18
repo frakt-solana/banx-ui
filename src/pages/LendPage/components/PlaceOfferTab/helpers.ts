1cf494c21dfbd3aa1d8511f3203d74d14075d1ed
iamsphere <ezekiel.9218@gmail.com>
2023-09-18 20:09:47 +0300

    Add calculateBestLoanValue helpers Set best offer value in first render

--- src/pages/LendPage/components/PlaceOfferTab/helpers.ts
+++ src/pages/LendPage/components/PlaceOfferTab/helpers.ts
@@ -15,3 +15,15 @@ export const shouldShowDepositError = ({
   const isDepositErrorVisible = totalAvailableFunds < offerSize
   return isDepositErrorVisible
 }
+
+const TRANSACTION_FEE = 0.01
+
+export const calculateBestLoanValue = (solanaBalance: number, bestOffer: number) => {
+  const balanceAfterDeductingFee = solanaBalance - TRANSACTION_FEE
+  const maxLoanValue = Math.max(balanceAfterDeductingFee, 0)
+
+  const bestOfferInSol = bestOffer / 1e9 || 0
+
+  const defaultLoanValue = Math.min(maxLoanValue, bestOfferInSol) || 0
+  return defaultLoanValue.toFixed(2)
+}
