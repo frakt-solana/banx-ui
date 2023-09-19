83fa03909b7fa710ebaafa87229c326e8cb29d26
sablevsky <vladislav1145@gmail.com>
2023-09-19 17:38:44 +0200

    Add Page styles for Refinance page

--- src/pages/RefinancePage/RefinancePage.tsx
+++ src/pages/RefinancePage/RefinancePage.tsx
@@ -1,11 +1,13 @@
 import RefinanceHeader from './components/RefinanceHeader'
 import { RefinanceTable } from './components/RefinanceTable'
 
+import styles from './RefinancePage.module.less'
+
 export const RefinancePage = () => {
   return (
-    <>
+    <div className={styles.pageWrapper}>
       <RefinanceHeader />
       <RefinanceTable />
-    </>
+    </div>
   )
 }
