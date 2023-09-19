b92dbc48aef003255ecc49c3c135af8024ccd68f
sablevsky <vladislav1145@gmail.com>
2023-09-19 17:43:46 +0200

    Add Summary in RefinanceTable

--- src/pages/RefinancePage/components/RefinanceTable/RefinanceTable.tsx
+++ src/pages/RefinancePage/components/RefinanceTable/RefinanceTable.tsx
@@ -5,12 +5,25 @@ import { useFakeInfinityScroll } from '@banx/hooks'
 import { PATHS } from '@banx/router'
 import { ViewState, useTableView } from '@banx/store'
 
+import { Summary } from './Summary'
 import { getTableColumns } from './columns'
 import { EMPTY_MESSAGE } from './constants'
 import { useRefinanceTable } from './hooks'
 
+import styles from './RefinanceTable.module.less'
+
 export const RefinanceTable = () => {
-  const { loans, sortViewParams, loading, showEmptyList } = useRefinanceTable()
+  const {
+    loans,
+    sortViewParams,
+    loading,
+    showEmptyList,
+    selectedLoans,
+    onSelectLoan,
+    onSelectAllLoans,
+    onDeselectAllLoans,
+    findSelectedLoan,
+  } = useRefinanceTable()
 
   const { viewState } = useTableView()
 
@@ -18,6 +31,8 @@ export const RefinanceTable = () => {
 
   const columns = getTableColumns({
     isCardView: viewState === ViewState.CARD,
+    onSelectLoan,
+    findSelectedLoan,
   })
 
   if (showEmptyList)
@@ -28,11 +43,19 @@ export const RefinanceTable = () => {
       <Table
         data={data}
         columns={columns}
+        className={styles.refinanceTable}
+        onRowClick={onSelectLoan}
         sortViewParams={sortViewParams}
         rowKeyField="publicKey"
         loading={loading}
         showCard
       />
       <div ref={fetchMoreTrigger} />
+      <Summary
+        selectedLoans={selectedLoans}
+        onSelectAllLoans={onSelectAllLoans}
+        onDeselectAllLoans={onDeselectAllLoans}
+      />
     </div>
+  )
 }
