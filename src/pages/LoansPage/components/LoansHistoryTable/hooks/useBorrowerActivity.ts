acf21dcefaeb53c99bf614a77dc23a3da8da4e85
iamsphere <ezekiel.9218@gmail.com>
2023-09-20 15:38:06 +0300

    Change cacheTime on acitivi queries

--- src/pages/LoansPage/components/LoansHistoryTable/hooks/useBorrowerActivity.ts
+++ src/pages/LoansPage/components/LoansHistoryTable/hooks/useBorrowerActivity.ts
@@ -45,7 +45,7 @@ export const useBorrowerActivity = () => {
       return lastPage.data?.length ? lastPage.pageParam + 1 : undefined
     },
     staleTime: 60 * 1000,
-    cacheTime: 100_000,
+    cacheTime: 1_000_000,
     networkMode: 'offlineFirst',
     refetchOnWindowFocus: false,
     enabled: !!publicKeyString,
