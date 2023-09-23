51f87f154a92553891b3281f1d23ac1df8f56b23
sablevsky <vladislav1145@gmail.com>
2023-09-23 13:16:07 +0200

    Add loader on AdventuresPage

--- src/pages/AdventuresPage/AdventuresPage.tsx
+++ src/pages/AdventuresPage/AdventuresPage.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 
+import { Loader } from '@banx/components/Loader'
 import { Tab, Tabs, useTabs } from '@banx/components/Tabs'
 
 import { AdventuresList } from './components/AdventuresList'
@@ -21,6 +22,7 @@ export const AdventuresPage: FC = () => {
     <div className={styles.pageWrapper}>
       <div className={styles.content}>
         <Header />
+        {isLoading && <Loader className={styles.loader} />}
         {!isLoading && !!adventuresInfo && (
           <>
             <Tabs value={currentTabValue} {...tabProps} />
