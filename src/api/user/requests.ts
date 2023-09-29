0f6a0f65f443e4f40a4b0cd25bb1fd121213726a
sablevsky <vladislav1145@gmail.com>
2023-09-29 12:28:36 +0200

    Add banx user notifications requests

--- src/api/user/requests.ts
+++ src/api/user/requests.ts
@@ -1,8 +1,137 @@
 import axios from 'axios'
+import { web3 } from 'fbonds-core'
 
 import { BACKEND_BASE_URL } from '@banx/constants'
+import { getDiscordAvatarUrl } from '@banx/utils'
 
-import { UserRewards } from './types'
+import { DiscordUserInfo, DiscordUserInfoRaw, UserRewards } from './types'
+
+type FetchDiscordUser = (props: { publicKey: web3.PublicKey }) => Promise<DiscordUserInfo | null>
+export const fetchDiscordUser: FetchDiscordUser = async ({ publicKey }) => {
+  try {
+    const { data } = await axios.get<DiscordUserInfoRaw>(
+      `${BACKEND_BASE_URL}/user/${publicKey.toBase58()}`,
+    )
+
+    if (!data) return null
+
+    const avatarUrl = getDiscordAvatarUrl(data.discordId, data.avatar)
+
+    if (!avatarUrl) return null
+
+    const { data: avatarExists } = await axios.get<string>(avatarUrl)
+
+    return {
+      avatarUrl: avatarExists ? avatarUrl : null,
+      isOnServer: data?.isOnServer ?? false,
+    }
+  } catch (error) {
+    return {
+      avatarUrl: null,
+      isOnServer: false,
+    }
+  }
+}
+
+type RemoveDiscordUser = (props: { publicKey: web3.PublicKey }) => Promise<void>
+export const removeDiscordUser: RemoveDiscordUser = async ({ publicKey }) => {
+  try {
+    await axios.get(`${BACKEND_BASE_URL}/user/${publicKey.toBase58()}/delete`)
+  } catch (error) {
+    return
+  }
+}
+
+type GetBanxUserNotifications = (props: {
+  publicKey: web3.PublicKey
+}) => Promise<ReadonlyArray<Notification>>
+export const getBanxUserNotifications: GetBanxUserNotifications = async ({ publicKey }) => {
+  try {
+    const { data } = await axios.get<ReadonlyArray<Notification>>(
+      `${BACKEND_BASE_URL}/history/${publicKey.toBase58()}`,
+    )
+
+    return data ?? []
+  } catch (error) {
+    return []
+  }
+}
+
+type MarkBanxNotificationsAsRead = (props: {
+  publicKey: web3.PublicKey
+  notificationIds: string[]
+}) => Promise<void>
+export const markBanxNotificationsAsRead: MarkBanxNotificationsAsRead = async ({
+  publicKey,
+  notificationIds,
+}) => {
+  await axios.post(`${BACKEND_BASE_URL}/history/${publicKey.toBase58()}`, {
+    ids: notificationIds,
+  })
+}
+
+type DeleteBanxNotifications = (props: {
+  publicKey: web3.PublicKey
+  notificationIds: string[]
+}) => Promise<void>
+export const deleteBanxNotifications: DeleteBanxNotifications = async ({
+  publicKey,
+  notificationIds,
+}) => {
+  await axios.post(`${BACKEND_BASE_URL}/history/${publicKey.toBase58()}/delete`, {
+    ids: notificationIds,
+  })
+}
+
+type GetBanxUserNotificationsSettings = (props: {
+  publicKey: web3.PublicKey
+}) => Promise<Record<string, boolean>>
+export const getBanxUserNotificationsSettings: GetBanxUserNotificationsSettings = async ({
+  publicKey,
+}) => {
+  const { data } = await axios.get<Record<string, boolean>>(
+    `${BACKEND_BASE_URL}/settings/${publicKey.toBase58()}`,
+  )
+
+  return data
+}
+
+type SetBanxUserNotificationsSettings = (props: {
+  publicKey: web3.PublicKey
+  settings: Record<string, boolean>
+}) => Promise<void>
+export const setBanxUserNotificationsSettings: SetBanxUserNotificationsSettings = async ({
+  publicKey,
+  settings,
+}) => {
+  await axios.post(`${BACKEND_BASE_URL}/settings/${publicKey.toBase58()}`, settings)
+}
+
+type BanxSignIn = (params: {
+  publicKey: web3.PublicKey
+  signature: string
+}) => Promise<string | null>
+export const banxSignIn: BanxSignIn = async ({ publicKey, signature }) => {
+  const { data } = await axios.post<{ access_token: string }>(`${BACKEND_BASE_URL}/auth/sign-in`, {
+    publicKey,
+    signature,
+  })
+
+  return data?.access_token ?? null
+}
+
+//TODO Not implemented on BE yet. Use instead of manual expiration check of access token
+type CheckBanxAccessToken = (token: string) => Promise<boolean>
+export const checkBanxAccessToken: CheckBanxAccessToken = async (token) => {
+  const { data } = await axios.post<{ token_valid: boolean }>(
+    `${BACKEND_BASE_URL}/auth/validate-token`,
+    {
+      token,
+    },
+  )
+
+  return data?.token_valid ?? false
+}
 
 type FetchUserRewards = (props: { publicKey: string }) => Promise<UserRewards>
 export const fetchUserRewards: FetchUserRewards = async ({ publicKey }) => {
--- src/api/user/types.ts
+++ src/api/user/types.ts
@@ -1,3 +1,34 @@
+export interface DiscordUserInfoRaw {
+  avatar: string
+  discordId: string
+  isOnServer: boolean
+}
+
+export interface DiscordUserInfo {
+  avatarUrl: string | null
+  isOnServer: boolean
+}
+
+export enum BanxNotificationType {
+  LOAN = 'loan',
+  DEPOSIT = 'deposit',
+  LOT_TICKET = 'lotTicket',
+  GRACE = 'grace',
+}
+
+export interface BanxNotification {
+  id: string
+  type: BanxNotificationType
+  user: string
+  message: {
+    title: string
+    body: string
+  }
+  image?: string
+  isRead: boolean
+  date: number
+}
+
 interface Rewards {
   user: string
   reward: number
