3208c2e4ecf82c3c3990766802bbd93d756632e0
sablevsky <vladislav1145@gmail.com>
2023-10-02 13:27:51 +0200

    Add uncontrolled behavior for Toggle

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -5,7 +5,7 @@ import classNames from 'classnames'
 import styles from './Toggle.module.less'
 
 export interface ToggleProps {
-  checked: boolean
+  checked?: boolean
   onChange: (value: boolean) => void
   label?: string
   defaultChecked?: boolean
@@ -19,14 +19,18 @@ export const Toggle: FC<ToggleProps> = ({
   defaultChecked,
   className,
 }) => {
+  const isControlled = typeof checked === 'boolean'
+
   return (
     <label className={classNames(styles.root, className)}>
       <input
         type="checkbox"
         className={styles.input}
         defaultChecked={defaultChecked}
-        checked={checked}
-        onChange={() => onChange(!checked)}
+        checked={isControlled ? checked : undefined}
+        onChange={(event) => {
+          onChange(isControlled ? !checked : event.target.checked)
+        }}
       />
       <span className={styles.slider} />
       <span className={styles.label}>{label}</span>
