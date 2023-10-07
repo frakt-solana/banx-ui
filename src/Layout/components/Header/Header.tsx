8e8a8867d6ffb647273962db529ec949344e8a2c
iamsphere <ezekiel.9218@gmail.com>
2023-10-07 12:04:39 +0400

    Add rewards button to root header component Add Cup icon

--- src/Layout/components/Header/Header.tsx
+++ src/Layout/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import { useWallet } from '@solana/wallet-adapter-react'
+import { NavLink } from 'react-router-dom'
 
 import { BanxNotificationsButton } from '@banx/components/BanxNotifications'
-import { WalletConnectButton } from '@banx/components/Buttons'
+import { Button, WalletConnectButton } from '@banx/components/Buttons'
 
-import { Logo, LogoFull } from '@banx/icons'
+import { Cup, Logo, LogoFull } from '@banx/icons'
+import { PATHS } from '@banx/router'
 
 import { BurgerIcon } from '../BurgerMenu/components'
 import ThemeSwitcher from '../ThemeSwitcher'
@@ -15,10 +17,18 @@ export const Header = () => {
 
   return (
     <div className={styles.header}>
-      <a href="/" className={styles.logoWrapper}>
-        <LogoFull className={styles.logo} />
-        <Logo className={styles.logoBasic} />
-      </a>
+      <div className={styles.logoWrapper}>
+        <NavLink to={PATHS.ROOT}>
+          <LogoFull className={styles.logo} />
+          <Logo className={styles.logoBasic} />
+        </NavLink>
+        <NavLink to={PATHS.LEADERBOARD}>
+          <Button className={styles.rewardsButton} type="circle" variant="text">
+            <Cup /> Rewards
+          </Button>
+        </NavLink>
+      </div>
+
       <div className={styles.widgetContainer}>
         <ThemeSwitcher />
         {connected && <BanxNotificationsButton />}
