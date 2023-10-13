dced60e5ee7d668ef4d620a0123cbc1557d7d483
iamsphere <ezekiel.9218@gmail.com>
2023-10-13 12:35:34 +0400

    prettier formatte code

--- src/pages/OffersPage/components/ActiveOffersTable/columns.tsx
+++ src/pages/OffersPage/components/ActiveOffersTable/columns.tsx
@@ -27,7 +27,12 @@ export const getTableColumns = ({ isCardView }: GetTableColumns) => {
     },
     {
       key: 'interest',
-      title: <HeaderCell label="Total claim" tooltipText="Sum of lent amount and accrued interest to date" />,
+      title: (
+        <HeaderCell
+          label="Total claim"
+          tooltipText="Sum of lent amount and accrued interest to date"
+        />
+      ),
       render: (_, loan) => <InterestCell loan={loan} />,
     },
     {
