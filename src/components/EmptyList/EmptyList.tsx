ed0583975a8282e501c1ce3de8655fbc30d42bf0
iamsphere <ezekiel.9218@gmail.com>
2023-10-16 01:23:43 +0400

    Modify EmptyList component, add onClick prop

--- src/components/EmptyList/EmptyList.tsx
+++ src/components/EmptyList/EmptyList.tsx
@@ -12,19 +12,24 @@ interface EmptyListProps {
   buttonText?: string
   path?: string
   className?: string
+  onClick?: () => void
 }
 
-const EmptyList: FC<EmptyListProps> = ({ message, buttonText, path, className }) => {
+const EmptyList: FC<EmptyListProps> = ({ message, buttonText, path = '', className, onClick }) => {
+  const button = (
+    <Button onClick={onClick ? onClick : undefined} className={styles.emptyListButton}>
+      {buttonText}
+    </Button>
+  )
+
+  const buttonElement = onClick ? button : <NavLink to={path}>{button}</NavLink>
+
   return (
     <div className={classNames(styles.wrapper, className)}>
       <div className={styles.emptyList}>
         <span className={styles.emptyListMessage}>{message}</span>
       </div>
-      {buttonText && path && (
-        <NavLink to={path} className={styles.emptyListButton}>
-          <Button>{buttonText}</Button>
-        </NavLink>
-      )}
+      {buttonText && buttonElement}
     </div>
   )
 }
