dbcabd8da431d40fc7eb383b44627588903a7cf3
iamsphere <ezekiel.9218@gmail.com>
2023-10-16 14:24:38 +0400

    Change twitter link

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,7 +3,7 @@ export const LEADERBOARD_URL = 'https://leaderboard.frakt.xyz/'
 export const TENSOR_BANX_MARKET_URL = 'https://www.tensor.trade/trade/banx/'
 export const SOLANAFM_URL = 'https://solana.fm/'
 
-export const TWITTER_URL = 'https://twitter.com/FRAKT_HQ'
+export const TWITTER_URL = 'https://twitter.com/banx_gg'
 export const GITHUB_URL = 'https://github.com/frakt-solana'
 export const MEDIUM_URL = 'https://medium.com/@banx_HQ'
 export const DOCS_URL = 'https://docs.banx.gg/'
