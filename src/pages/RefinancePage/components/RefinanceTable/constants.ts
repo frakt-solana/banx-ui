4a63bb556683fd29b5f38de89af68be035f5d8bb
iamsphere <ezekiel.9218@gmail.com>
2023-10-18 20:53:14 +0400

    Add max apr to refinance loans

--- src/pages/RefinancePage/components/RefinanceTable/constants.ts
+++ src/pages/RefinancePage/components/RefinanceTable/constants.ts
@@ -3,6 +3,8 @@ export const SECONDS_IN_72_HOURS = 72 * SECONDS_IN_HOUR
 
 export const INCREASE_PERCENT_APR_PER_HOUR = 1
 
+export const MAX_INCREASE_PERCENT = 253
+
 export const DEFAULT_SORT_OPTION = { label: 'Ends in', value: 'duration_asc' }
 
 export const EMPTY_MESSAGE =
