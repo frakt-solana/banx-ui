d7704cdc266753ff8de0c517054f3b0d7e777be2
iamsphere <ezekiel.9218@gmail.com>
2023-10-20 11:03:00 +0400

    Show highlight colors only if LiquidationLoansCount exists Rename "Liquidation during 24h" => "Liquidating within 24h"

--- src/pages/DashboardPage/components/DashboardBorrowTab/components/MyLoans/constants.ts
+++ src/pages/DashboardPage/components/DashboardBorrowTab/components/MyLoans/constants.ts
@@ -13,7 +13,7 @@ export const STATUS_COLOR_MAP: Record<LoansStatus, string> = {
 export const STATUS_DISPLAY_NAMES: Record<LoansStatus, string> = {
   [LoansStatus.Active]: 'Active Loans',
   [LoansStatus.Terminating]: 'Terminating Loans',
-  [LoansStatus.Liquidation]: 'Liquidation during 24h',
+  [LoansStatus.Liquidation]: 'Liquidating within 24h',
 }
 
 export const NO_DATA_CHART_DATA = {
