2e161ece1b6e365f312ba4da6a923986377bb175
iamsphere <ezekiel.9218@gmail.com>
2023-11-01 11:59:12 +0400

    Update sdk, add makeCreateBondingOfferAction, makeUpdateBondingOfferAction

---
+++ src/transactions/bonds/makeCreateBondingOfferAction.ts
@@ -0,0 +1,59 @@
+import { web3 } from 'fbonds-core'
+import { BondOfferOptimistic } from 'fbonds-core/lib/fbond-protocol/functions/perpetual'
+//TODO: Update imports in SDK
+import { createPerpetualBondOfferBonding } from 'fbonds-core/lib/fbond-protocol/functions/perpetual/offer/createPerpetualBondOfferBonding'
+import { BondingCurveType } from 'fbonds-core/lib/fbond-protocol/types'
+
+import { BONDS } from '@banx/constants'
+import { sendTxnPlaceHolder } from '@banx/utils'
+
+import { MakeActionFn } from '../TxnExecutor'
+
+export type MakeCreateBondingOfferActionParams = {
+  marketPubkey: string
+  loanValue: number //? value in sol
+  loansAmount: number
+  deltaValue: number //? value in sol
+  bondingCurveType?: BondingCurveType
+}
+
+export type MakeCreateBondingOfferAction = MakeActionFn<
+  MakeCreateBondingOfferActionParams,
+  BondOfferOptimistic
+>
+
+export const makeCreateBondingOfferAction: MakeCreateBondingOfferAction = async (
+  ixnParams,
+  { connection, wallet },
+) => {
+  const {
+    marketPubkey,
+    loanValue,
+    loansAmount,
+    bondingCurveType = BondingCurveType.Linear,
+    deltaValue,
+  } = ixnParams
+
+  const { instructions, signers, optimisticResult } = await createPerpetualBondOfferBonding({
+    programId: new web3.PublicKey(BONDS.PROGRAM_PUBKEY),
+    connection,
+    accounts: {
+      hadoMarket: new web3.PublicKey(marketPubkey),
+      userPubkey: wallet.publicKey as web3.PublicKey,
+    },
+    args: {
+      loanValue: loanValue * 1e9,
+      delta: deltaValue * 1e9,
+      quantityOfLoans: loansAmount,
+      bondingCurveType,
+    },
+    sendTxn: sendTxnPlaceHolder,
+  })
+
+  return {
+    instructions,
+    signers,
+    additionalResult: optimisticResult,
+    lookupTables: [],
+  }
+}
---
+++ src/transactions/bonds/makeUpdateBondingOfferAction.ts
@@ -0,0 +1,50 @@
+import { web3 } from 'fbonds-core'
+import { BondOfferOptimistic } from 'fbonds-core/lib/fbond-protocol/functions/perpetual'
+//TODO: Update imports in SDK
+import { updatePerpetualOfferBonding } from 'fbonds-core/lib/fbond-protocol/functions/perpetual/offer/updatePerpetualOfferBonding'
+
+import { BONDS } from '@banx/constants'
+import { sendTxnPlaceHolder } from '@banx/utils'
+
+import { MakeActionFn } from '../TxnExecutor'
+
+export type MakeUpdateBondingOfferActionParams = {
+  loanValue: number //? value in sol
+  loansAmount: number
+  deltaValue: number //? value in sol
+  offerPubkey: string
+}
+
+export type MakeUpdateBondingOfferAction = MakeActionFn<
+  MakeUpdateBondingOfferActionParams,
+  BondOfferOptimistic
+>
+
+export const makeUpdateBondingOfferAction: MakeUpdateBondingOfferAction = async (
+  ixnParams,
+  { connection, wallet },
+) => {
+  const { loanValue, loansAmount, deltaValue, offerPubkey } = ixnParams
+
+  const { instructions, signers, optimisticResult } = await updatePerpetualOfferBonding({
+    programId: new web3.PublicKey(BONDS.PROGRAM_PUBKEY),
+    connection,
+    accounts: {
+      bondOfferV2: new web3.PublicKey(offerPubkey),
+      userPubkey: wallet.publicKey as web3.PublicKey,
+    },
+    args: {
+      loanValue: loanValue * 1e9,
+      delta: deltaValue * 1e9,
+      quantityOfLoans: loansAmount,
+    },
+    sendTxn: sendTxnPlaceHolder,
+  })
+
+  return {
+    instructions,
+    signers,
+    additionalResult: optimisticResult,
+    lookupTables: [],
+  }
+}
