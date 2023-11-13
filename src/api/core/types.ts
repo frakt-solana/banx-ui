8b71bc225c692c3318b7935b2069fecde3f90483
iamsphere <ezekiel.9218@gmail.com>
2023-11-13 12:26:37 +0400

    Add Fire icon and add isHot type to MarketPreviewSchema

--- src/api/core/types.ts
+++ src/api/core/types.ts
@@ -18,6 +18,7 @@ export const MarketPreviewSchema = z
     activeBondsAmount: z.number(),
     activeOfferAmount: z.number(),
     loansTvl: z.number(),
+    isHot: z.boolean(),
   })
   .merge(MarketMetaSchema)
 
