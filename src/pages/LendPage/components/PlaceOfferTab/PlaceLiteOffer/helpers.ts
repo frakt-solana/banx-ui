cabc39807f4c4e55267e74b87bc505d124dcd2df
iamsphere <ezekiel.9218@gmail.com>
2023-11-22 10:57:42 +0400

    Remove unnecessary default value in bestOfferInSol calculation Add comment to explain value in transaction fee

--- src/pages/LendPage/components/PlaceOfferTab/PlaceLiteOffer/helpers.ts
+++ src/pages/LendPage/components/PlaceOfferTab/PlaceLiteOffer/helpers.ts
@@ -1,12 +1,12 @@
 import { MarketPreview } from '@banx/api/core'
 
-const TRANSACTION_FEE_IN_SOL = 0.01
+const TRANSACTION_FEE_IN_SOL = 0.01 //? transaction fee for prevent any case with not enough sol
 
 export const calculateBestLoanValue = (solanaBalance: number, bestOffer: number) => {
   const balanceAfterDeductingFee = solanaBalance - TRANSACTION_FEE_IN_SOL
   const maxLoanValue = Math.max(balanceAfterDeductingFee, 0)
 
-  const bestOfferInSol = bestOffer / 1e9 || 0
+  const bestOfferInSol = bestOffer / 1e9
 
   const defaultLoanValue = Math.min(maxLoanValue, bestOfferInSol) || 0
   return defaultLoanValue.toFixed(2)
