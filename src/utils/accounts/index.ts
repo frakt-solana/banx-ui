8c41f100a622c91befdf59e6fc53af87c7c35d2d
iamsphere <ezekiel.9218@gmail.com>
2023-11-23 12:46:35 +0400

    Add isLive prop to subscribe on onAccountChange to prevent rerenders bug

--- src/utils/accounts/index.ts
+++ src/utils/accounts/index.ts
@@ -3,9 +3,8 @@ import { useEffect, useState } from 'react'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import { web3 } from 'fbonds-core'
 
-const useNativeAccount = (): {
-  account: web3.AccountInfo<Buffer> | null
-} => {
+type UseNativeAccount = (isLive: boolean) => web3.AccountInfo<Buffer> | null
+const useNativeAccount: UseNativeAccount = (isLive) => {
   const { connection } = useConnection()
   const { wallet, publicKey } = useWallet()
 
@@ -21,18 +20,21 @@ const useNativeAccount = (): {
         setNativeAccount(acc)
       }
     })
-    connection.onAccountChange(publicKey, (acc) => {
-      if (acc) {
-        setNativeAccount(acc)
-      }
-    })
-  }, [wallet, publicKey, connection])
 
-  return { account: nativeAccount }
+    if (isLive) {
+      connection.onAccountChange(publicKey, (acc) => {
+        if (acc) {
+          setNativeAccount(acc)
+        }
+      })
+    }
+  }, [wallet, publicKey, connection, isLive])
+
+  return nativeAccount
 }
 
-export const useSolanaBalance = () => {
-  const { account } = useNativeAccount()
+export const useSolanaBalance = (isLive = true) => {
+  const account = useNativeAccount(isLive)
 
   const balance = (account?.lamports || 0) / web3.LAMPORTS_PER_SOL
 
