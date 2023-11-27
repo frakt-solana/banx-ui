2cc2b9be7f10988a02232b42ad0086d2056f5a30
sablevsky <vladislav1145@gmail.com>
2023-11-27 11:31:36 +0100

    Remove unused wallets

--- src/constants/wallets.ts
+++ src/constants/wallets.ts
@@ -1,26 +1,4 @@
-import { SentreWalletAdapter } from '@sentre/connector'
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
-import { ExodusWalletAdapter } from '@solana/wallet-adapter-exodus'
 import { GlowWalletAdapter } from '@solana/wallet-adapter-glow'
-import { SlopeWalletAdapter } from '@solana/wallet-adapter-slope'
 import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
-import { SolletWalletAdapter } from '@solana/wallet-adapter-sollet'
-import {
-  CoinbaseWalletAdapter,
-  LedgerWalletAdapter,
-  MathWalletAdapter,
-  TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets'
 
-export const WALLETS = [
-  new SolflareWalletAdapter(),
-  new SlopeWalletAdapter(),
-  new GlowWalletAdapter(),
-  new LedgerWalletAdapter(),
-  new CoinbaseWalletAdapter(),
-  new TorusWalletAdapter(),
-  new MathWalletAdapter(),
-  new ExodusWalletAdapter(),
-  new SentreWalletAdapter(),
-  new SolletWalletAdapter({ network: WalletAdapterNetwork.Mainnet }),
-]
+export const WALLETS = [new SolflareWalletAdapter(), new GlowWalletAdapter()]
