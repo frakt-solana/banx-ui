cf4cd2cdabb93560dd89d07b641480679a5e580e
Vladislav <24393100+sablevsky@users.noreply.github.com>
2023-11-27 13:46:16 +0300

    BAN-1639: Update wallet adapters (#364)
    * Update wallet adapters
    
    * Remove unused wallets

--- src/constants/wallets.ts
+++ src/constants/wallets.ts
@@ -1,26 +1,4 @@
-import { SentreWalletAdapter } from '@sentre/connector'
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
-import {
-  CoinbaseWalletAdapter,
-  ExodusWalletAdapter,
-  GlowWalletAdapter,
-  LedgerWalletAdapter,
-  MathWalletAdapter,
-  SlopeWalletAdapter,
-  SolflareWalletAdapter,
-  SolletWalletAdapter,
-  TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets'
+import { GlowWalletAdapter } from '@solana/wallet-adapter-glow'
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
 
-export const WALLETS = [
-  new SolflareWalletAdapter(),
-  new SlopeWalletAdapter(),
-  new GlowWalletAdapter(),
-  new LedgerWalletAdapter(),
-  new CoinbaseWalletAdapter(),
-  new TorusWalletAdapter(),
-  new MathWalletAdapter(),
-  new ExodusWalletAdapter(),
-  new SentreWalletAdapter(),
-  new SolletWalletAdapter({ network: WalletAdapterNetwork.Mainnet }),
-]
+export const WALLETS = [new SolflareWalletAdapter(), new GlowWalletAdapter()]
