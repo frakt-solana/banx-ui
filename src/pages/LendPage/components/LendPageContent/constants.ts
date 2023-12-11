22aaf0dc74163482f468a183a269c68a5ec5739d
Vladislav <60342317+iamsphere@users.noreply.github.com>
2023-12-11 14:32:11 +0300

    BAN-1624: New wordings on Lend page (#418)
    * Rename IN LOANS -> "Loans TVL"
    Rename IN OFFERS -> "Offers TVL"
    Rename Offer TVL -> Offers TVL
    
    * Return old labels names

--- src/pages/LendPage/components/LendPageContent/constants.ts
+++ src/pages/LendPage/components/LendPageContent/constants.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_SORT_OPTION = { label: 'Loans TVL', value: 'loansTvl_desc' }
 
 export const SORT_OPTIONS = [
-  { label: 'Offer TVL', value: 'offerTvl' },
+  { label: 'Offers TVL', value: 'offerTvl' },
   { label: 'Loans TVL', value: 'loansTvl' },
   { label: 'Active loans', value: 'activeLoans' },
   { label: 'APR', value: 'apr' },
