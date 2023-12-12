9e005eb1f4056f6dce1f1246570143f8143365a8
sablevsky <vladislav1145@gmail.com>
2023-12-12 13:58:41 +0100

    Change RPC

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,7 +1,7 @@
 export const RPC_ENDPOINTS = [
   process.env.RPC_LOCALHOST || '',
-  'https://mainnet.helius-rpc.com/?api-key=f9beff77-c671-473d-98d2-cd0af1ee0a5a',
+  'https://mainnet.helius-rpc.com/?api-key=d283785c-6f2c-43de-b460-a938327fc6f5',
   'https://smart-powerful-knowledge.solana-mainnet.discover.quiknode.pro/887d337202841789c5a6d168e1c3b7809e45d268/',
   'https://solana-mainnet.rpc.extrnode.com/',
-  'https://rpc.hellomoon.io/51e91377-d146-4f74-bf41-c4b11d42cebe'
+  'https://rpc.hellomoon.io/51e91377-d146-4f74-bf41-c4b11d42cebe',
 ].filter(Boolean)
