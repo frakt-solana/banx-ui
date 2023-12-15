70938058e4d8eb31cf47d9b16439390bd90439ae
sablevsky <vladislav1145@gmail.com>
2023-12-15 15:05:17 +0100

    Add gap between paragraphs

--- src/components/modals/OnboardingModal/content.tsx
+++ src/components/modals/OnboardingModal/content.tsx
@@ -14,31 +14,37 @@ const DASHBOARD_CONTENT = {
     {
       img: <img className={styles.slideImg} src={dashboardImg1} alt="dashboard 1" />,
       text: (
-        <p className={styles.slideText}>
-          Banx is a blazing fast{' '}
-          <span className={styles.slideTextImportant}>P2P NFT lending protocol</span>, with lower
-          loan-to-value ratios, lower liquidation risk, and the lowest interest rates.
-        </p>
+        <div className={styles.slideText}>
+          <p>
+            Banx is a blazing fast{' '}
+            <span className={styles.slideTextImportant}>P2P NFT lending protocol</span>, with lower
+            loan-to-value ratios, lower liquidation risk, and the lowest interest rates.
+          </p>
+        </div>
       ),
     },
     {
       img: <img className={styles.slideImg} src={dashboardImg2} alt="dashboard 2" />,
       text: (
-        <p className={styles.slideText}>
-          <span className={styles.slideTextImportant}>Loans are perpetual</span>: they last at least
-          3 days and don&apos;t expire if you keep them healthy with small repayments.
-        </p>
+        <div className={styles.slideText}>
+          <p>
+            <span className={styles.slideTextImportant}>Loans are perpetual</span>: they last at
+            least 3 days and don&apos;t expire if you keep them healthy with small repayments.
+          </p>
+        </div>
       ),
     },
     {
       img: <img className={styles.slideImg} src={dashboardImg3} alt="dashboard 3" />,
       text: (
-        <p className={styles.slideText}>
-          The $BANX token will hit the market in Q1&apos;t24 and{' '}
-          <span className={styles.slideTextImportant}>
-            you can farm it on banx.gg with no vesting.
-          </span>
-        </p>
+        <div className={styles.slideText}>
+          <p>
+            The $BANX token will hit the market in Q1&apos;t24 and{' '}
+            <span className={styles.slideTextImportant}>
+              you can farm it on banx.gg with no vesting.
+            </span>
+          </p>
+        </div>
       ),
     },
   ],
@@ -50,13 +56,16 @@ const BORROW_CONTENT = {
     {
       img: <img className={styles.slideImg} src={borrowImg1} alt="borrow 1" />,
       text: (
-        <p className={styles.slideText}>
-          <span className={styles.slideTextImportant}>Banx loans are perpetual</span>: they have no
-          expiry, no fixed duration.
-          <br />
-          As the borrower, you can repay and exit any time. Interest accrues to the second so you
-          never pay more than you need to.
-        </p>
+        <div className={styles.slideText}>
+          <p>
+            <span className={styles.slideTextImportant}>Banx loans are perpetual</span>: they have
+            no expiry, no fixed duration.
+          </p>
+          <p>
+            As the borrower, you can repay and exit any time. Interest accrues to the second so you
+            never pay more than you need to.
+          </p>
+        </div>
       ),
     },
   ],
@@ -68,13 +77,16 @@ const LEND_CONTENT = {
     {
       img: <img className={styles.slideImg} src={lendImg1} alt="lend 1" />,
       text: (
-        <p className={styles.slideText}>
-          As a lender, you can instantly earn yield by funding loans in the refinance auctions. Or
-          you can lend via the orderbook, where you control exactly how much you offer.
-          <br />
-          You can automate your lending pools to reduce offer size the more you lend, and to place
-          offers back in the orderbook as they are repaid.
-        </p>
+        <div className={styles.slideText}>
+          <p>
+            As a lender, you can instantly earn yield by funding loans in the refinance auctions. Or
+            you can lend via the orderbook, where you control exactly how much you offer.
+          </p>
+          <p>
+            You can automate your lending pools to reduce offer size the more you lend, and to place
+            offers back in the orderbook as they are repaid.
+          </p>
+        </div>
       ),
     },
   ],
@@ -86,13 +98,16 @@ const REFINANCE_CONTENT = {
     {
       img: <img className={styles.slideImg} src={refinanceImg1} alt="refinance 1" />,
       text: (
-        <p className={styles.slideText}>
-          Lenders can terminate loans if they wish to exit. The loan will be sent to the refinance
-          auction, where it will be offered to other lenders on the same terms.
-          <br />
-          The auction continues until a new lender refinances, or the borrower repays in full. If
-          neither happens after 72 hours, the NFT will be liquidated to the lender.
-        </p>
+        <div className={styles.slideText}>
+          <p>
+            Lenders can terminate loans if they wish to exit. The loan will be sent to the refinance
+            auction, where it will be offered to other lenders on the same terms.
+          </p>
+          <p>
+            The auction continues until a new lender refinances, or the borrower repays in full. If
+            neither happens after 72 hours, the NFT will be liquidated to the lender.
+          </p>
+        </div>
       ),
     },
   ],
