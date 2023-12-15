c93088bf0e43a57bcdabb58b1306c61c18cea587
sablevsky <vladislav1145@gmail.com>
2023-12-15 15:01:51 +0100

    Implement carousel arrows

--- src/components/modals/OnboardingModal/OnboardingModal.tsx
+++ src/components/modals/OnboardingModal/OnboardingModal.tsx
@@ -5,6 +5,8 @@ import { Carousel } from 'antd'
 import { Loader } from '@banx/components/Loader'
 import { Modal } from '@banx/components/modals/BaseModal'
 
+import { ChevronDown } from '@banx/icons'
+
 import { CONTENT } from './content'
 import { OnboardingModalContentType } from './types'
 
@@ -25,7 +27,13 @@ export const OnboardingModal: FC<OnboardingModalProps> = ({
     <Modal open centered maskClosable={false} width={572} footer={false} onCancel={onCancel}>
       <div className={styles.modalContent}>
         <h3 className={styles.title}>{content.title}</h3>
-        <Carousel draggable>
+        <Carousel
+          draggable
+          nextArrow={<ChevronDown className={styles.carouselArrow} />}
+          prevArrow={<ChevronDown className={styles.carouselArrow} />}
+          arrows
+          infinite={false}
+        >
           {content.slides.map(({ text, img }, idx) => (
             <div className={styles.slide} key={idx}>
               {img}
