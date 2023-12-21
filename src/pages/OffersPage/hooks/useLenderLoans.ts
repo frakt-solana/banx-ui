2ac25ac5b4bef2c7709d7bb3f5559785fa5f1943
sablevsky <vladislav1145@gmail.com>
2023-12-21 10:29:22 +0100

    Finish useLenderLoans hook

--- src/pages/OffersPage/hooks/useLenderLoans.ts
+++ src/pages/OffersPage/hooks/useLenderLoans.ts
@@ -1,8 +1,8 @@
 import { useMemo } from 'react'
 
-// import { useWallet } from '@solana/wallet-adapter-react'
+import { useWallet } from '@solana/wallet-adapter-react'
 import { useQuery } from '@tanstack/react-query'
-import { web3 } from 'fbonds-core'
+import { chain, maxBy } from 'lodash'
 
 import { Loan, fetchLenderLoans } from '@banx/api/core'
 
@@ -11,25 +11,13 @@ import { useHiddenNftsMints, useLenderLoansOptimistic } from '.'
 export const USE_LENDER_LOANS_QUERY_KEY = 'lenderLoansV2'
 
 export const useLenderLoans = () => {
-  // const { publicKey } = useWallet()
-  // const { publicKey: publicKey2 } = useWallet()
-  const publicKey = useMemo(
-    () => new web3.PublicKey('mm8fDa7jiufFGD6h4foq9vdmTRxDeDSTKB9CZynwQQs'),
-    [],
-  )
+  const { publicKey } = useWallet()
   const publicKeyString = publicKey?.toBase58() || ''
 
-  const { /*mints,*/ addMints } = useHiddenNftsMints()
+  const { mints: hiddenLoansMints, addMints } = useHiddenNftsMints()
+  const { loans: optimisticLoans, addLoans, findLoan, updateLoans } = useLenderLoansOptimistic()
 
-  const { /*loans: optimisticLoans,*/ addLoans, findLoan, updateLoans } = useLenderLoansOptimistic()
-  // const { optimisticOffers, remove: removeOffers, update: updateOrAddOffer } = useOffersOptimistic()
-
-  const {
-    data: loans,
-    isLoading,
-    // isFetching,
-    // isFetched,
-  } = useQuery(
+  const { data: loans, isLoading } = useQuery(
     [USE_LENDER_LOANS_QUERY_KEY, publicKeyString],
     () => fetchLenderLoans({ walletPublicKey: publicKeyString, getAll: true }),
     {
@@ -39,66 +27,24 @@ export const useLenderLoans = () => {
     },
   )
 
-  // const walletOptimisticLoans = useMemo(() => {
-  //   if (!publicKeyString) return []
-  //   return optimisticLoans.filter(({ wallet }) => wallet === publicKeyString)
-  // }, [optimisticLoans, publicKeyString])
+  const walletOptimisticLoans = useMemo(() => {
+    if (!publicKeyString) return []
+    return optimisticLoans.filter(({ wallet }) => wallet === publicKeyString)
+  }, [optimisticLoans, publicKeyString])
 
-  // const { data, fetchNextPage, isFetchingNextPage, hasNextPage, isLoading } = useInfiniteQuery({
-  //   queryKey: [USE_LENDER_LOANS_QUERY_KEY, publicKey],
-  //   queryFn: ({ pageParam = 0 }) => fetchData(pageParam),
-  //   getPreviousPageParam: (firstPage) => {
-  //     return firstPage.pageParam - 1 ?? undefined
-  //   },
-  //   getNextPageParam: (lastPage) => {
-  //     return lastPage.data?.length ? lastPage.pageParam + 1 : undefined
-  //   },
-  //   refetchOnWindowFocus: false,
-  // })
+  const mergedLoans = useMemo(() => {
+    if (isLoading || !loans) {
+      return []
+    }
 
-  // const loans = useMemo(() => {
-  //   return data?.pages?.map((page) => page.data).flat() || []
-  // }, [data])
-
-  // const { data, isLoading, isFetching, isFetched } = useQuery(
-  //   [USE_LENDER_LOANS_QUERY_KEY, publicKeyString],
-  //   () => fetchLenderLoansAndOffers({ walletPublicKey: publicKeyString }),
-  //   {
-  //     enabled: !!publicKeyString,
-  //     refetchOnWindowFocus: false,
-  //     refetchInterval: 30 * 1000,
-  //   },
-  // )
-
-  // const walletOptimisticLoans = useMemo(() => {
-  //   if (!publicKeyString) return []
-  //   return optimisticLoans.filter(({ wallet }) => wallet === publicKeyString)
-  // }, [optimisticLoans, publicKeyString])
-
-  //? Check same active loans (duplicated with BE) and purge them
-  // useEffect(() => {
-  //   if (!data || isFetching || !isFetched || !publicKeyString) return
-
-  //   const expiredLoans = walletOptimisticLoans.filter((loan) =>
-  //     isOptimisticLoanExpired(loan, publicKeyString),
-  //   )
-
-  //   const optimisticsToRemove = walletOptimisticLoans.filter(({ loan }) => {
-  //     const sameLoanFromBE = (data?.nfts || []).find(
-  //       ({ publicKey }) => publicKey === loan.publicKey,
-  //     )
-  //     if (!sameLoanFromBE) return false
-  //     const isBELoanNewer = isLoanNewer(sameLoanFromBE, loan)
-  //     return isBELoanNewer
-  //   })
-
-  //   if (optimisticsToRemove.length || expiredLoans.length) {
-  //     removeOptimisticLoans(
-  //       map([...expiredLoans, ...optimisticsToRemove], ({ loan }) => loan.publicKey),
-  //       publicKeyString,
-  //     )
-  //   }
-  // }, [data, isFetched, publicKeyString, walletOptimisticLoans, removeOptimisticLoans, isFetching])
+    return chain(loans)
+      .concat(walletOptimisticLoans.map(({ loan }) => loan))
+      .groupBy('publicKey')
+      .map((loans) => maxBy(loans, ({ fraktBond }) => fraktBond.lastTransactedAt))
+      .compact()
+      .filter((loan) => !hiddenLoansMints.includes(loan.nft.mint))
+      .value()
+  }, [loans, isLoading, walletOptimisticLoans, hiddenLoansMints])
 
   const updateOrAddLoan = (loan: Loan) => {
     const loanExists = !!findLoan(loan.publicKey, publicKeyString)
@@ -106,12 +52,9 @@ export const useLenderLoans = () => {
   }
 
   return {
-    loans: loans || [],
+    loans: mergedLoans || [],
     loading: isLoading,
     updateOrAddLoan,
-    // fetchNextPage,
-    // isFetchingNextPage,
-    // hasNextPage,
     addMints,
   }
 }
