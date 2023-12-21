03fe034cf947b41c35710f7f9bf0ed06d08a7915
sablevsky <vladislav1145@gmail.com>
2023-12-21 10:31:09 +0100

    Finish ManageModal logic

--- src/pages/OffersPage/components/ActiveTabContent/components/LoansTable/TableCells/ActionsCell/ManageModal.tsx
+++ src/pages/OffersPage/components/ActiveTabContent/components/LoansTable/TableCells/ActionsCell/ManageModal.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo, useState } from 'react'
+import { FC, useMemo } from 'react'
 
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import classNames from 'classnames'
@@ -7,26 +7,15 @@ import { TxnExecutor } from 'solana-transactions-executor'
 
 import { Button } from '@banx/components/Buttons'
 import { Loader } from '@banx/components/Loader'
-import { Slider } from '@banx/components/Slider'
-import { StatInfo, VALUES_TYPES } from '@banx/components/StatInfo'
-import { Tab, Tabs, useTabs } from '@banx/components/Tabs'
 import { Modal } from '@banx/components/modals/BaseModal'
 
 import { Loan } from '@banx/api/core'
 import { useMarketOffers } from '@banx/pages/LendPage'
-import { findBestOffer, useHiddenNftsMints, useLenderLoans } from '@banx/pages/OffersPage'
+import { findBestOffer, useLenderLoans } from '@banx/pages/OffersPage'
 import { useModal } from '@banx/store'
 import { defaultTxnErrorHandler } from '@banx/transactions'
 import { makeInstantRefinanceAction, makeTerminateAction } from '@banx/transactions/loans'
-import {
-  HealthColorIncreasing,
-  calculateLoanRepayValue,
-  enqueueSnackbar,
-  getColorByPercent,
-  isLoanActiveOrRefinanced,
-  isLoanTerminating,
-  trackPageEvent,
-} from '@banx/utils'
+import { enqueueSnackbar, isLoanActiveOrRefinanced, isLoanTerminating } from '@banx/utils'
 
 import styles from './ActionsCell.module.less'
 
@@ -37,36 +26,36 @@ interface ManageModalProps {
 export const ManageModal: FC<ManageModalProps> = ({ loan }) => {
   const { close } = useModal()
 
-  const modalTabs: Tab[] = [
-    {
-      label: 'Repayment call',
-      value: 'repayment',
-      disabled: true, //TODO enable when repayment call implemented
-    },
-    {
-      label: 'Closure',
-      value: 'closure',
-      // disabled: !onTerminate && !onInstant,
-    },
-  ]
-
-  const defaultTabValue = modalTabs[1].value
-
-  const {
-    tabs,
-    value: tabValue,
-    setValue: setTabValue,
-  } = useTabs({
-    tabs: modalTabs,
-    defaultValue: defaultTabValue,
-  })
+  //? Uncomment when repayment call ready
+  // const modalTabs: Tab[] = [
+  //   {
+  //     label: 'Repayment call',
+  //     value: 'repayment',
+  //     disabled: true, //TODO enable when repayment call implemented
+  //   },
+  //   {
+  //     label: 'Closure',
+  //     value: 'closure',
+  //     // disabled: !onTerminate && !onInstant,
+  //   },
+  // ]
+  // const defaultTabValue = modalTabs[1].value
+  // const {
+  //   tabs,
+  //   value: tabValue,
+  //   setValue: setTabValue,
+  // } = useTabs({
+  //   tabs: modalTabs,
+  //   defaultValue: defaultTabValue,
+  // })
 
   return (
     <Modal className={styles.modal} open onCancel={close} width={572}>
-      <Tabs className={styles.tabs} tabs={tabs} value={tabValue} setValue={setTabValue} />
-
+      {/* //? Uncomment when repayment call ready */}
+      {/* <Tabs className={styles.tabs} tabs={tabs} value={tabValue} setValue={setTabValue} />
       {tabValue === modalTabs[0].value && <RepaymentCallContent loan={loan} close={close} />}
-      {tabValue === modalTabs[1].value && <ClosureContent loan={loan} />}
+      {tabValue === modalTabs[1].value && <ClosureContent loan={loan} />} */}
+      <ClosureContent loan={loan} />
     </Modal>
   )
 }
@@ -78,8 +67,7 @@ const ClosureContent: FC<ClosureContentProps> = ({ loan }) => {
   const wallet = useWallet()
   const { connection } = useConnection()
 
-  const { updateOrAddLoan } = useLenderLoans()
-  const { addMints: hideLoans } = useHiddenNftsMints()
+  const { updateOrAddLoan, addMints: hideLoans } = useLenderLoans()
 
   const { offers, updateOrAddOffer, isLoading } = useMarketOffers({
     marketPubkey: loan.fraktBond.hadoMarket,
@@ -90,9 +78,9 @@ const ClosureContent: FC<ClosureContentProps> = ({ loan }) => {
   }, [offers, loan, wallet])
 
   const loanActiveOrRefinanced = isLoanActiveOrRefinanced(loan)
-  const hasRefinanceOffers = !isEmpty(bestOffer)
+  const hasRefinanceOffer = !isEmpty(bestOffer)
 
-  const canRefinance = hasRefinanceOffers && loanActiveOrRefinanced
+  const canRefinance = hasRefinanceOffer && loanActiveOrRefinanced
   const canTerminate = !isLoanTerminating(loan) && loanActiveOrRefinanced
 
   const terminateLoan = () => {
@@ -182,62 +170,63 @@ const ClosureContent: FC<ClosureContentProps> = ({ loan }) => {
   )
 }
 
-interface RepaymentCallContentProps {
-  loan: Loan
-  close: () => void
-}
-const RepaymentCallContent: FC<RepaymentCallContentProps> = ({ loan, close }) => {
-  const DEFAULT_PERCENT_VALUE = 25
+//? Uncomment when repayment call ready
+// interface RepaymentCallContentProps {
+//   loan: Loan
+//   close: () => void
+// }
+// const RepaymentCallContent: FC<RepaymentCallContentProps> = ({ loan, close }) => {
+//   const DEFAULT_PERCENT_VALUE = 25
 
-  const totalClaim = calculateLoanRepayValue(loan)
-  const initialRepayValue = totalClaim * (DEFAULT_PERCENT_VALUE / 100)
+//   const totalClaim = calculateLoanRepayValue(loan)
+//   const initialRepayValue = totalClaim * (DEFAULT_PERCENT_VALUE / 100)
 
-  const [partialPercent, setPartialPercent] = useState<number>(DEFAULT_PERCENT_VALUE)
-  const [paybackValue, setPaybackValue] = useState<number>(initialRepayValue)
+//   const [partialPercent, setPartialPercent] = useState<number>(DEFAULT_PERCENT_VALUE)
+//   const [paybackValue, setPaybackValue] = useState<number>(initialRepayValue)
 
-  const onPartialPercentChange = (percentValue: number) => {
-    setPartialPercent(percentValue)
-    setPaybackValue((totalClaim * percentValue) / 100)
-  }
+//   const onPartialPercentChange = (percentValue: number) => {
+//     setPartialPercent(percentValue)
+//     setPaybackValue((totalClaim * percentValue) / 100)
+//   }
 
-  const remainingDebt = totalClaim - paybackValue
+//   const remainingDebt = totalClaim - paybackValue
 
-  const ltv = (remainingDebt / loan.nft.collectionFloor) * 100
-  const colorLTV = getColorByPercent(ltv, HealthColorIncreasing)
+//   const ltv = (remainingDebt / loan.nft.collectionFloor) * 100
+//   const colorLTV = getColorByPercent(ltv, HealthColorIncreasing)
 
-  const onSend = () => {
-    try {
-      //TODO send repayment call logic here
-      trackPageEvent('myoffers', 'activetab-repaymentcall')
-    } finally {
-      close()
-    }
-  }
+//   const onSend = () => {
+//     try {
+//       //TODO send repayment call logic here
+//       trackPageEvent('myoffers', 'activetab-repaymentcall')
+//     } finally {
+//       close()
+//     }
+//   }
 
-  return (
-    <div className={styles.modalContent}>
-      <StatInfo
-        flexType="row"
-        label="Total claim:"
-        value={totalClaim}
-        divider={1e9}
-        classNamesProps={{ container: styles.repaymentCallInfo }}
-      />
-      <Slider value={partialPercent} onChange={onPartialPercentChange} />
-      <div className={styles.repaimentCallAdditionalInfo}>
-        <StatInfo flexType="row" label="Repay value" value={paybackValue} divider={1e9} />
-        <StatInfo flexType="row" label="Remaining debt" value={remainingDebt} divider={1e9} />
-        <StatInfo
-          flexType="row"
-          label="New LTV"
-          value={ltv}
-          valueStyles={{ color: colorLTV }}
-          valueType={VALUES_TYPES.PERCENT}
-        />
-      </div>
-      <Button className={styles.repaymentCallButton} onClick={onSend} disabled={!partialPercent}>
-        Send
-      </Button>
-    </div>
-  )
-}
+//   return (
+//     <div className={styles.modalContent}>
+//       <StatInfo
+//         flexType="row"
+//         label="Total claim:"
+//         value={totalClaim}
+//         divider={1e9}
+//         classNamesProps={{ container: styles.repaymentCallInfo }}
+//       />
+//       <Slider value={partialPercent} onChange={onPartialPercentChange} />
+//       <div className={styles.repaimentCallAdditionalInfo}>
+//         <StatInfo flexType="row" label="Repay value" value={paybackValue} divider={1e9} />
+//         <StatInfo flexType="row" label="Remaining debt" value={remainingDebt} divider={1e9} />
+//         <StatInfo
+//           flexType="row"
+//           label="New LTV"
+//           value={ltv}
+//           valueStyles={{ color: colorLTV }}
+//           valueType={VALUES_TYPES.PERCENT}
+//         />
+//       </div>
+//       <Button className={styles.repaymentCallButton} onClick={onSend} disabled={!partialPercent}>
+//         Send
+//       </Button>
+//     </div>
+//   )
+// }
