417fb85a634b4644567355754131254b86ed86b1
sablevsky <vladislav1145@gmail.com>
2024-01-17 15:03:23 +0100

    Change tooltip text

--- src/pages/BorrowPage/components/BorrowTable/Summary/Summary.tsx
+++ src/pages/BorrowPage/components/BorrowTable/Summary/Summary.tsx
@@ -118,7 +118,7 @@ export const Summary: FC<SummaryProps> = ({
           {!!showBonkRewardsSticker && (
             <Tooltip
               className={styles.bonkTokenSticker}
-              title="You are getting 50% cashback on platform fees in $BONK!"
+              title="50% upfront fee refunded in $BONK"
             >
               <img src={bonkTokenImg} alt="Bonk token sticker" />
             </Tooltip>
