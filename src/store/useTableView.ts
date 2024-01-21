23f42fd83afbac3fefc2c8f7d71e11cfced60b2f
iamsphere <ezekiel.9218@gmail.com>
2024-01-21 16:18:07 +0400

    Implement save to localStorage in useTableView

--- src/store/useTableView.ts
+++ src/store/useTableView.ts
@@ -12,12 +12,22 @@ interface TableViewState {
   setViewState: (nextValue: ViewState) => void
 }
 
+const VIEW_STORAGE_KEY = '@banx.viewState'
+
 export const useTableView = create<TableViewState>((set) => {
   const isMobileWidth = window.innerWidth < MOBILE_WIDTH
 
+  const savedViewState = localStorage.getItem(VIEW_STORAGE_KEY)
+
+  const defaultViewState = isMobileWidth ? ViewState.CARD : ViewState.TABLE
+  const initialViewState = savedViewState ? JSON.parse(savedViewState) : defaultViewState
+
   const initialState: TableViewState = {
-    viewState: isMobileWidth ? ViewState.CARD : ViewState.TABLE,
-    setViewState: (nextValue) => set((state) => ({ ...state, viewState: nextValue })),
+    viewState: initialViewState,
+    setViewState: (nextValue) => {
+      localStorage.setItem(VIEW_STORAGE_KEY, JSON.stringify(nextValue))
+      set((state) => ({ ...state, viewState: nextValue }))
+    },
   }
 
   return initialState
