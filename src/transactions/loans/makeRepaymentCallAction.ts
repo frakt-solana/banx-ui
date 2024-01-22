84de8e608928ec5a395a6d8f5a19901e842f2236
sablevsky <vladislav1145@gmail.com>
2024-01-22 16:56:40 +0100

    Implement makeRepaymentCallAction

---
+++ src/transactions/loans/makeRepaymentCallAction.ts
@@ -0,0 +1,57 @@
+import { web3 } from 'fbonds-core'
+import { setRepaymentCall } from 'fbonds-core/lib/fbond-protocol/functions/perpetual'
+import { RepaymentCall } from 'fbonds-core/lib/fbond-protocol/types'
+import { MakeActionFn } from 'solana-transactions-executor'
+
+import { Loan } from '@banx/api/core'
+import { BONDS } from '@banx/constants'
+import { sendTxnPlaceHolder } from '@banx/utils'
+
+export type MakeRepaymentCallParams = {
+  loan: Loan
+  callAmount: number //? SOL lamports
+}
+
+export type MakeRepaymentCallAction = MakeActionFn<MakeRepaymentCallParams, Loan>
+
+export const makeRepaymentCallAction: MakeRepaymentCallAction = async (
+  { loan, callAmount },
+  { connection, wallet },
+) => {
+  const { bondTradeTransaction, fraktBond, repaymentCall } = loan
+
+  const {
+    instructions,
+    signers,
+    optimistic: optimisticResult,
+  } = await setRepaymentCall({
+    programId: new web3.PublicKey(BONDS.PROGRAM_PUBKEY),
+
+    args: {
+      callAmount,
+    },
+    optimistic: {
+      repaymentCall: repaymentCall as RepaymentCall,
+    },
+    accounts: {
+      bondTradeTransaction: new web3.PublicKey(bondTradeTransaction.publicKey),
+      fraktBond: new web3.PublicKey(fraktBond.publicKey),
+      userPubkey: wallet.publicKey as web3.PublicKey,
+    },
+
+    connection,
+    sendTxn: sendTxnPlaceHolder,
+  })
+
+  const optimisticLoan: Loan = {
+    ...loan,
+    repaymentCall: optimisticResult.repaymentCall,
+  }
+
+  return {
+    instructions,
+    signers,
+    additionalResult: optimisticLoan,
+    lookupTables: [],
+  }
+}
