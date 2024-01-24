f38f0ceb59a09ab058175edf7612a07a437d2c7b
sablevsky <vladislav1145@gmail.com>
2024-01-24 16:05:05 +0100

    Implement RepaymentCall logic in RepayModal

--- src/pages/LoansPage/components/LoansActiveTable/TableCells/ActionsCell/RepayModal.tsx
+++ src/pages/LoansPage/components/LoansActiveTable/TableCells/ActionsCell/RepayModal.tsx
@@ -8,7 +8,7 @@ import { Modal } from '@banx/components/modals/BaseModal'
 
 import { Loan } from '@banx/api/core'
 import { useModal } from '@banx/store'
-import { calculateLoanRepayValue, trackPageEvent } from '@banx/utils'
+import { calculateLoanRepayValue, getColorByPercent, trackPageEvent } from '@banx/utils'
 
 import { useLoansTransactions } from '../../hooks'
 
@@ -21,19 +21,35 @@ interface RepayModalProps {
 export const RepayModal: FC<RepayModalProps> = ({ loan }) => {
   const { close } = useModal()
 
+  const repaymentCallExists = !!loan.repaymentCall?.callAmount
+
+  const totalRepayValue = calculateLoanRepayValue(loan)
+
+  const DEFAULT_REPAY_PERCENT = 100
+  const initialRepayPercent = repaymentCallExists
+    ? ((loan.repaymentCall?.callAmount || 0) / totalRepayValue) * 100
+    : DEFAULT_REPAY_PERCENT
+
+  const initialRepayValue = repaymentCallExists
+    ? loan.repaymentCall?.callAmount || 0
+    : totalRepayValue * (initialRepayPercent / 100)
+
   const { repayLoan, repayPartialLoan } = useLoansTransactions()
 
-  const initialRepayValue = calculateLoanRepayValue(loan)
-
-  const [partialPercent, setPartialPercent] = useState<number>(100)
+  const [partialPercent, setPartialPercent] = useState<number>(initialRepayPercent)
   const [paybackValue, setPaybackValue] = useState<number>(initialRepayValue)
 
   const onPartialPercentChange = (percentValue: number) => {
     setPartialPercent(percentValue)
-    setPaybackValue((initialRepayValue * percentValue) / 100)
+    setPaybackValue((totalRepayValue * percentValue) / 100)
   }
 
-  const remainingValue = initialRepayValue - paybackValue
+  const remainingValue = totalRepayValue - paybackValue
+
+  const colorClassNameByValue = {
+    [Math.ceil(initialRepayPercent)]: styles.repayModalSliderYellow,
+    100: styles.repayModalSliderGreen,
+  }
 
   const onSubmit = async () => {
     try {
@@ -52,19 +68,25 @@ export const RepayModal: FC<RepayModalProps> = ({ loan }) => {
 
   return (
     <Modal open onCancel={close}>
-      <StatInfo
-        flexType="row"
-        label="Debt:"
-        value={initialRepayValue}
-        divider={1e9}
-        classNamesProps={{ container: styles.repayModalInfo }}
-      />
       <Slider
         value={partialPercent}
         onChange={onPartialPercentChange}
         className={styles.repayModalSlider}
+        rootClassName={getColorByPercent(partialPercent, colorClassNameByValue)}
       />
       <div className={styles.repayModalAdditionalInfo}>
+        {repaymentCallExists && (
+          <StatInfo
+            flexType="row"
+            label="Repayment call"
+            value={loan.repaymentCall?.callAmount || 0}
+            divider={1e9}
+            classNamesProps={{ label: styles.repayModalRepaymentCall }}
+            onClickProps={{
+              onLabelClick: () => onPartialPercentChange(initialRepayPercent),
+            }}
+          />
+        )}
         <StatInfo flexType="row" label="Repay value" value={paybackValue} divider={1e9} />
         <StatInfo flexType="row" label="Remaining debt" value={remainingValue} divider={1e9} />
       </div>
