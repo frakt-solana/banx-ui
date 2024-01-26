fe66da094faccd130a36ea545722e2bee08180cb
sablevsky <vladislav1145@gmail.com>
2024-01-26 19:18:46 +0100

    Add new calculations for ManageModal

--- src/pages/OffersPage/components/ActiveTabContent/components/LoansTable/TableCells/ActionsCell/helpers.ts
+++ src/pages/OffersPage/components/ActiveTabContent/components/LoansTable/TableCells/ActionsCell/helpers.ts
@@ -1,22 +1,35 @@
+import { calculateCurrentInterestSolPure } from 'fbonds-core/lib/fbond-protocol/functions/perpetual'
+import moment from 'moment'
+
 import { Loan } from '@banx/api/core'
-import { calculateLoanRepayValue } from '@banx/utils'
+import { calculateLoanRepayValue, isLoanRepaymentCallActive } from '@banx/utils'
 
 export const calculateRepaymentStaticValues = (loan: Loan) => {
+  const repaymentCallActive = isLoanRepaymentCallActive(loan)
   const repaymentCallAmount = loan.repaymentCall?.callAmount || 0
 
   const totalClaim = calculateLoanRepayValue(loan)
 
+  const { solAmount, feeAmount, soldAt, amountOfBonds } = loan.bondTradeTransaction
+  const accuredInterest = calculateCurrentInterestSolPure({
+    loanValue: solAmount + feeAmount,
+    startTime: soldAt,
+    currentTime: moment().unix(),
+    rateBasePoints: amountOfBonds,
+  })
+
   const DEFAULT_REPAY_PERCENT = 50
-  const initialRepayPercent = repaymentCallAmount
+  const initialRepayPercent = repaymentCallActive
     ? (repaymentCallAmount / totalClaim) * 100
     : DEFAULT_REPAY_PERCENT
 
-  const initialRepayValue = repaymentCallAmount
+  const initialRepayValue = repaymentCallActive
     ? repaymentCallAmount
     : totalClaim * (initialRepayPercent / 100)
 
   return {
-    repaymentCallExists: !!repaymentCallAmount,
+    repaymentCallActive,
+    accuredInterest,
     totalClaim,
     initialRepayPercent,
     initialRepayValue,
