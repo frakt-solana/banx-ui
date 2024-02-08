67bf59403e01396a9572fb7d9dfa810a4d803b95
sablevsky <vladislav1145@gmail.com>
2024-02-08 17:53:54 +0100

    Implement refinancing into the most suitable offer

--- src/pages/LoansPage/components/LoansActiveTable/TableCells/ActionsCell/RefinanceModal.tsx
+++ src/pages/LoansPage/components/LoansActiveTable/TableCells/ActionsCell/RefinanceModal.tsx
@@ -1,18 +1,21 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useMemo, useState } from 'react'
 
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import classNames from 'classnames'
+import { chain } from 'lodash'
 import { TxnExecutor } from 'solana-transactions-executor'
 
 import { Button } from '@banx/components/Buttons'
+import { Loader } from '@banx/components/Loader'
 import { Slider } from '@banx/components/Slider'
 import { createPercentValueJSX, createSolValueJSX } from '@banx/components/TableComponents'
 import { Modal } from '@banx/components/modals/BaseModal'
 
-import { Loan, Offer } from '@banx/api/core'
+import { Loan } from '@banx/api/core'
 import { BONDS } from '@banx/constants'
+import { useMarketOffers } from '@banx/pages/LendPage'
 import { useSelectedLoans } from '@banx/pages/LoansPage/loansState'
-import { useLoansOptimistic, useModal, useOffersOptimistic } from '@banx/store'
+import { useLoansOptimistic, useModal } from '@banx/store'
 import { defaultTxnErrorHandler } from '@banx/transactions'
 import { makeBorrowRefinanceAction } from '@banx/transactions/loans'
 import {
@@ -20,7 +23,10 @@ import {
   calculateApr,
   calculateLoanRepayValue,
   enqueueSnackbar,
+  filterOutWalletLoans,
+  findSuitableOffer,
   isLoanTerminating,
+  isOfferNotEmpty,
   trackPageEvent,
 } from '@banx/utils'
 
@@ -28,21 +34,40 @@ import styles from './ActionsCell.module.less'
 
 interface RefinanceModalProps {
   loan: Loan
-  offer?: Offer
 }
 
-export const RefinanceModal: FC<RefinanceModalProps> = ({ loan, offer }) => {
-  const { bondTradeTransaction, fraktBond, nft } = loan
-  const { feeAmount } = bondTradeTransaction
-
+export const RefinanceModal: FC<RefinanceModalProps> = ({ loan }) => {
   const { close } = useModal()
   const wallet = useWallet()
   const { connection } = useConnection()
 
+  const { bondTradeTransaction, fraktBond, nft } = loan
+  const { feeAmount } = bondTradeTransaction
+
+  const { offers, updateOrAddOffer, isLoading } = useMarketOffers({
+    marketPubkey: loan.fraktBond.hadoMarket,
+  })
+
+  const bestOffer = useMemo(() => {
+    return chain(offers)
+      .sortBy(({ currentSpotPrice }) => currentSpotPrice)
+      .filter(isOfferNotEmpty)
+      .reverse()
+      .value()
+      .at(0)
+  }, [offers])
+
+  const initialCurrentSpotPrice = useMemo(() => {
+    if (!bestOffer) return 0
+    return bestOffer.currentSpotPrice
+  }, [bestOffer])
+
+  useEffect(() => {
+    setCurrentSpotPrice(initialCurrentSpotPrice)
+  }, [initialCurrentSpotPrice])
+
   const { update: updateLoansOptimistic } = useLoansOptimistic()
   const { clear: clearSelection } = useSelectedLoans()
-
-  const { update: updateOptimisticOffers } = useOffersOptimistic()
 
   const isTerminatingStatus = isLoanTerminating(loan)
 
@@ -50,8 +75,6 @@ export const RefinanceModal: FC<RefinanceModalProps> = ({ loan, offer }) => {
 
   const currentLoanBorrowedAmount = calcLoanBorrowedAmount(loan)
   const currentLoanDebt = calculateLoanRepayValue(loan)
-
-  const initialCurrentSpotPrice = offer?.currentSpotPrice || 0
 
   const currentApr = bondTradeTransaction.amountOfBonds
 
@@ -75,20 +98,36 @@ export const RefinanceModal: FC<RefinanceModalProps> = ({ loan, offer }) => {
   const differenceToPay = newLoanDebt - currentLoanDebt
 
   const refinance = () => {
-    if (!offer) return
+    if (!bestOffer) return
     trackPageEvent('myloans', isTerminatingStatus ? 'refinance' : 'reborrow')
 
-    const newOffer = { ...offer, currentSpotPrice }
-    const newLoan = {
-      ...loan,
-      bondTradeTransaction: { ...bondTradeTransaction, amountOfBonds: newApr },
-    }
+    const suitableOffer = chain(offers)
+      .thru((offers) =>
+        filterOutWalletLoans({
+          offers,
+          walletPubkey: wallet?.publicKey?.toBase58(),
+        }),
+      )
+      .thru((offers) =>
+        findSuitableOffer({
+          loanValue: currentSpotPrice,
+          offers,
+        }),
+      )
+      .value()
+
+    if (!suitableOffer) return
 
     new TxnExecutor(makeBorrowRefinanceAction, { connection, wallet })
-      .addTxnParam({ loan: newLoan, offer: newOffer })
+      .addTxnParam({
+        loan,
+        offer: suitableOffer,
+        solToRefinance: currentSpotPrice,
+        aprRate: newApr,
+      })
       .on('pfSuccessEach', (results) => {
         const { result, txnHash } = results[0]
-        result?.offer && updateOptimisticOffers([result.offer])
+        result?.offer && updateOrAddOffer(result.offer)
         if (result?.loan) {
           updateLoansOptimistic([result.loan], wallet.publicKey?.toBase58() || '')
         }
@@ -112,37 +151,42 @@ export const RefinanceModal: FC<RefinanceModalProps> = ({ loan, offer }) => {
 
   return (
     <Modal open onCancel={close}>
-      <LoanInfo
-        title="Current loan"
-        borrowedAmount={currentLoanBorrowedAmount}
-        debt={currentLoanDebt}
-        apr={currentApr}
-        className={styles.currentLoanInfo}
-        faded
-      />
-      <LoanInfo
-        title="New loan"
-        borrowedAmount={newLoanBorrowedAmount}
-        debt={newLoanDebt}
-        apr={newApr}
-        className={styles.newLoanInfo}
-      />
+      {isLoading && <Loader />}
+      {!isLoading && (
+        <>
+          <LoanInfo
+            title="Current loan"
+            borrowedAmount={currentLoanBorrowedAmount}
+            debt={currentLoanDebt}
+            apr={currentApr}
+            className={styles.currentLoanInfo}
+            faded
+          />
+          <LoanInfo
+            title="New loan"
+            borrowedAmount={newLoanBorrowedAmount}
+            debt={newLoanDebt}
+            apr={newApr}
+            className={styles.newLoanInfo}
+          />
 
-      <LoanDifference difference={differenceToPay} className={styles.difference} />
+          <LoanDifference difference={differenceToPay} className={styles.difference} />
 
-      <Slider
-        label="Loan"
-        value={partialPercent}
-        onChange={onPartialPercentChange}
-        className={styles.refinanceModalSlider}
-        marks={DEFAULT_SLIDER_MARKS}
-        min={10}
-        max={100}
-      />
+          <Slider
+            label="Loan"
+            value={partialPercent}
+            onChange={onPartialPercentChange}
+            className={styles.refinanceModalSlider}
+            marks={DEFAULT_SLIDER_MARKS}
+            min={10}
+            max={100}
+          />
 
-      <Button className={styles.refinanceModalButton} onClick={refinance} disabled={!offer}>
-        {isTerminatingStatus ? 'Extend' : 'Reborrow'}
-      </Button>
+          <Button className={styles.refinanceModalButton} onClick={refinance} disabled={!bestOffer}>
+            {isTerminatingStatus ? 'Extend' : 'Reborrow'}
+          </Button>
+        </>
+      )}
     </Modal>
   )
 }
