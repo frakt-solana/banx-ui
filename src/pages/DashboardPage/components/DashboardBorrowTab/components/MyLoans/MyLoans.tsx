542a3f969f41fb6b87451211401f0e9bc279e679
iamsphere <ezekiel.9218@gmail.com>
2024-02-26 12:45:23 +0400

    Add weekly interest to dasboard

--- src/pages/DashboardPage/components/DashboardBorrowTab/components/MyLoans/MyLoans.tsx
+++ src/pages/DashboardPage/components/DashboardBorrowTab/components/MyLoans/MyLoans.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 
 import { Button } from '@banx/components/Buttons'
 import { Doughnut } from '@banx/components/Charts'
+import { StatInfo } from '@banx/components/StatInfo'
 
 import { TotalBorrowerStats } from '@banx/api/stats'
 
@@ -18,7 +19,7 @@ interface MyLoansProps {
 const MyLoans: FC<MyLoansProps> = ({ stats }) => {
   const { loansData, chartData, buttonProps } = useMyLoans(stats)
 
-  const { totalBorrowed = 0, totalDebt = 0 } = stats || {}
+  const { totalBorrowed = 0, totalDebt = 0, totalWeeklyInterest = 0 } = stats || {}
 
   return (
     <div className={styles.loansContainer}>
@@ -26,8 +27,22 @@ const MyLoans: FC<MyLoansProps> = ({ stats }) => {
       <div className={styles.loansContent}>
         <div className={styles.loansStatsContainer}>
           <div className={styles.loansStats}>
-            <DashboardStatInfo label="Total borrowed" value={totalBorrowed} divider={1e9} />
-            <DashboardStatInfo label="Total debt" value={totalDebt} divider={1e9} />
+            <DashboardStatInfo
+              label="Total debt"
+              value={totalDebt}
+              classNamesProps={{ container: styles.totalDebt }}
+              divider={1e9}
+            />
+            <div className={styles.separateLine} />
+            <div className={styles.additionalStats}>
+              <StatInfo flexType="row" label="Total borrowed" value={totalBorrowed} divider={1e9} />
+              <StatInfo
+                flexType="row"
+                label="Weekly interest"
+                value={totalWeeklyInterest}
+                divider={1e9}
+              />
+            </div>
           </div>
           <div className={styles.mobileChart}>
             <Doughnut {...chartData} />
