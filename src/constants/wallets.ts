272921e08c601d09eb98b505e93ea1ac87947bfb
Vladislav <60342317+iamsphere@users.noreply.github.com>
2024-03-14 10:24:39 +0300

    Fix bug with missing metamask in WalletConnect (#638)

--- src/constants/wallets.ts
+++ src/constants/wallets.ts
@@ -1,7 +1,8 @@
 import { MagicEdenWalletAdapter } from '@solana/wallet-adapter-magiceden'
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
 
 //? Add additional wallet adapters here
-export const WALLETS = [new MagicEdenWalletAdapter()]
+export const WALLETS = [new MagicEdenWalletAdapter(), new SolflareWalletAdapter()]
 
 export const MAGIC_EDEN_WALLET_NAME = 'Magic Eden'
 export const MAGIC_EDEN_DOWNLOAD_URL =
