2e027b26130083fd50b2a29b2783fb41e0558857
iamsphere <ezekiel.9218@gmail.com>
2024-03-28 11:47:10 +0400

    Remove unnecessary markets from MARKETS_WITH_CUSTOM_APR

--- src/constants/loans.ts
+++ src/constants/loans.ts
@@ -1,10 +1,4 @@
 export const MARKETS_WITH_CUSTOM_APR: Record<string, number> = {
   ['oeN7bBJAjFqnvFwALPBazEFJeTi9F6tLvKHhuoJjSkk']: 3380, //? ONE collection PUBLIC market
   ['2RSYRoqZ2bKnKGeLtGFyTmDLNEaV9Z9zhmZd5qXxAsr9']: 3380, //? ONE collection PRIVATE market
-
-  ['HpzJyoNhfwbtco6Z8ghtqf85cDwFpYWqkBo1X1ah8cwX']: 3380, //? Lifinity collection PUBLIC market
-  ['FFhTvrX3L3QJwZrbjb2BQxV1PXccXfW8cWpz7VVyD3nM']: 3380, //? Lifinity collection PRIVATE market
-
-  ['4XNFTPM6v5fDySb3nsmfcozc2Cf12Xsmvy3nufNfoWUS']: 3380, //? Cloak DAO collection PUBLIC market
-  ['HuKW1yTTLFcTdt9cKvGxSBjYhmQgyAXpgyEwqdxAZS7e']: 3380, //? Cloak DAO collection PRIVATE market
 }
