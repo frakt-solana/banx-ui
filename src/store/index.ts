1e0e755c7a3c6a850af2d5132e5c128ef6a7e371
sablevsky <vladislav1145@gmail.com>
2024-04-03 15:31:35 +0200

    Implement usePriorityFees store hook

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ export { useIsLedger } from './useIsLedger'
 export { useBanxLogin, getBanxJwtFromLS } from './useBanxLogin'
 export * from './useModal'
 export * from './useTableView'
+export * from './usePriorityFees'
 
 export * from './markets'
 export * from './offers'
