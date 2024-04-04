c753548bc881ab54bfd61d902e6daf0225bab12a
iamsphere <ezekiel.9218@gmail.com>
2024-04-04 14:15:21 +0400

    Implmenet useTokenBalance hook, add USDC to tokens constants

--- src/utils/accounts/index.ts
+++ src/utils/accounts/index.ts
@@ -3,6 +3,8 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { web3 } from 'fbonds-core'
 
+import { BONDS } from '@banx/constants'
+
 type UseNativeAccount = ({ isLive }: { isLive?: boolean }) => web3.AccountInfo<Buffer> | null
 
 const useNativeAccount: UseNativeAccount = ({ isLive = true }) => {
@@ -27,11 +29,40 @@ const useNativeAccount: UseNativeAccount = ({ isLive = true }) => {
   return nativeAccount
 }
 
+export const useTokenBalance = (tokenAddress: string, { isLive = true }) => {
+  const { connection } = useConnection()
+  const { publicKey } = useWallet()
+
+  const { data: tokenBalance } = useQuery(
+    ['tokenBalance', publicKey, tokenAddress],
+    async () => {
+      if (connection && publicKey && tokenAddress) {
+        const tokenPublicKey = new web3.PublicKey(tokenAddress)
+        const tokenAccounts = await connection.getTokenAccountsByOwner(publicKey, {
+          programId: new web3.PublicKey(BONDS.PROGRAM_PUBKEY),
+          mint: tokenPublicKey,
+        })
+
+        const userTokenAccountAddress = tokenAccounts.value[0]?.pubkey
+        const balance = await connection.getTokenAccountBalance(userTokenAccountAddress)
+        return parseFloat(balance.value.amount)
+      }
+      return null
+    },
+    {
+      enabled: Boolean(connection && publicKey && tokenAddress),
+      refetchInterval: isLive ? 10000 : false,
+    },
+  )
+
+  return tokenBalance || 0
+}
+
 type UseSolanaBalance = (options?: { isLive?: boolean }) => number
 export const useSolanaBalance: UseSolanaBalance = ({ isLive = true } = {}) => {
   const account = useNativeAccount({ isLive })
 
-  const balance = (account?.lamports || 0) / web3.LAMPORTS_PER_SOL
+  const balance = account?.lamports || 0
 
   return balance
 }
