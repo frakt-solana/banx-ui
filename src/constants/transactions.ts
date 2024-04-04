df54777f9c2d18c7c2b1478b61b19872a4693fb4
sablevsky <vladislav1145@gmail.com>
2024-04-04 16:37:46 +0200

    Change TXN_EXECUTOR_CONFIRM_OPTIONS

--- src/constants/transactions.ts
+++ src/constants/transactions.ts
@@ -2,12 +2,12 @@ import { web3 } from 'fbonds-core'
 
 export const TXN_EXECUTOR_CONFIRM_OPTIONS: {
   skipPreflight: boolean
-  maxRetries: number
+  maxRetries: number | undefined
   commitment: web3.Commitment
   preflightCommitment: web3.Commitment
 } = {
-  skipPreflight: false,
-  maxRetries: 1,
-  commitment: 'confirmed',
-  preflightCommitment: 'confirmed',
+  skipPreflight: true,
+  maxRetries: 0,
+  commitment: 'processed',
+  preflightCommitment: 'processed',
 }
