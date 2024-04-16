fe5ea822dca33536993d298b39c31f15bfce7e0c
Vladislav <60342317+iamsphere@users.noreply.github.com>
2024-04-16 12:26:20 +0400

    BAN-2382: Improve lend page. Allow to open only one market (#751)
    * Remove unnecessary files (markets hooks and components in  LendPageContent)
    
    * Remove opened quety params, remove scroll to lend card, refactor
    Fix bug with unnecessary rerenders

--- src/store/markets/useMarketsURLControl.ts
+++ src/store/markets/useMarketsURLControl.ts
@@ -1,27 +1,19 @@
 import { useEffect } from 'react'
 
 import { useLocation, useNavigate } from 'react-router-dom'
-
-import { useMarketVisibility } from './useMarketVisibilityState'
-import { useSearchSelectedMarkets } from './useSearchMarketsURLControl'
+import { create } from 'zustand'
 
 export const useMarketsURLControl = (shouldControlQueryParams?: boolean) => {
   const location = useLocation()
   const navigate = useNavigate()
 
-  const { visibleMarkets, toggleMarketVisibility, setMarketVisibility } = useMarketVisibility()
   const { selectedMarkets, setSelectedMarkets } = useSearchSelectedMarkets()
 
   useEffect(() => {
     //? Markets query parameters should only be on certain pages.
     if (shouldControlQueryParams) {
       const queryParams = new URLSearchParams(location.search)
-      queryParams.delete('opened')
       queryParams.delete('collections')
-
-      if (visibleMarkets.length > 0) {
-        queryParams.append('opened', visibleMarkets.join(','))
-      }
 
       if (selectedMarkets.length > 0) {
         queryParams.append('collections', selectedMarkets.join(','))
@@ -29,20 +21,36 @@ export const useMarketsURLControl = (shouldControlQueryParams?: boolean) => {
 
       navigate({ search: queryParams.toString() }, { replace: true })
     }
-  }, [
-    navigate,
-    visibleMarkets,
-    selectedMarkets,
-    location.search,
-    location.pathname,
-    shouldControlQueryParams,
-  ])
+  }, [navigate, selectedMarkets, location.search, location.pathname, shouldControlQueryParams])
 
   return {
-    visibleMarkets,
-    toggleMarketVisibility,
     selectedMarkets,
     setSelectedMarkets,
-    setMarketVisibility,
   }
 }
+
+type SeatchSelectedMarketsState = {
+  selectedMarkets: string[]
+  setSelectedMarkets: (value: string[]) => void
+}
+
+export const useSearchSelectedMarkets = create<SeatchSelectedMarketsState>((set) => {
+  const initialState: SeatchSelectedMarketsState = {
+    selectedMarkets: [],
+    setSelectedMarkets: (value) => {
+      set(() => {
+        return { selectedMarkets: value }
+      })
+    },
+  }
+
+  const queryParams = new URLSearchParams(window.location.search)
+  const selectedMarketsParam = queryParams.get('collections')
+
+  if (selectedMarketsParam) {
+    const selectedMarkets = selectedMarketsParam.split(',')
+    initialState.selectedMarkets = selectedMarkets
+  }
+
+  return initialState
+})
