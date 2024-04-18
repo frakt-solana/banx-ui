683f72706289901f00fecbf2263395f171541247
iamsphere <ezekiel.9218@gmail.com>
2024-04-18 19:50:36 +0400

    Implement BorrowCard component without expanded content

--- src/pages/BorrowPage/RequestLoansContent/RequestLoansContent.tsx
+++ src/pages/BorrowPage/RequestLoansContent/RequestLoansContent.tsx
@@ -1,3 +1,48 @@
+import { useState } from 'react'
+
+import { isEmpty } from 'lodash'
+
+import { Loader } from '@banx/components/Loader'
+
+import { useFakeInfinityScroll } from '@banx/hooks'
+import { useMarketsPreview } from '@banx/pages/LendPage'
+
+import BorrowCard from './components/BorrowCard'
+
+import styles from './RequestLoansContent.module.less'
+
 export const RequestLoansContent = () => {
-  return <div>Request loans content here</div>
+  const { marketsPreview, isLoading: isLoadingMarkets } = useMarketsPreview()
+
+  const { data: markets, fetchMoreTrigger } = useFakeInfinityScroll({ rawData: marketsPreview })
+
+  const [visibleMarketPubkey, setMarketPubkey] = useState('')
+
+  const onCardClick = (marketPubkey: string) => {
+    const isSameMarketPubkey = visibleMarketPubkey === marketPubkey
+    const nextValue = !isSameMarketPubkey ? marketPubkey : ''
+    return setMarketPubkey(nextValue)
+  }
+
+  const isLoading = isLoadingMarkets && isEmpty(marketsPreview)
+
+  return (
+    <div className={styles.content}>
+      {isLoading && <Loader />}
+
+      {!isLoading && (
+        <div className={styles.marketsList}>
+          {markets.map((market) => (
+            <BorrowCard
+              key={market.marketPubkey}
+              market={market}
+              onClick={() => onCardClick(market.marketPubkey)}
+              isOpen={visibleMarketPubkey === market.marketPubkey}
+            />
+          ))}
+          <div ref={fetchMoreTrigger} />
+        </div>
+      )}
+    </div>
+  )
 }
---
+++ src/pages/BorrowPage/RequestLoansContent/components/BorrowCard/BorrowCard.tsx
@@ -0,0 +1,91 @@
+import React, { FC } from 'react'
+
+import classNames from 'classnames'
+
+import { Button } from '@banx/components/Buttons'
+import { TensorLink } from '@banx/components/SolanaLinks'
+import { StatInfo, VALUES_TYPES } from '@banx/components/StatInfo'
+import { DisplayValue } from '@banx/components/TableComponents'
+
+import { MarketPreview } from '@banx/api/core'
+import { ChevronDown } from '@banx/icons'
+
+import styles from './BorrowCard.module.less'
+
+interface BorrowCardProps {
+  market: MarketPreview
+  onClick: () => void
+  isOpen: boolean
+}
+
+const BorrowCard: FC<BorrowCardProps> = ({ market, onClick, isOpen }) => {
+  return (
+    <div className={styles.card}>
+      <div className={classNames(styles.cardBody, { [styles.opened]: isOpen })} onClick={onClick}>
+        <MarketMainInfo market={market} />
+        <div className={styles.additionalContentWrapper}>
+          <MarketAdditionalInfo market={market} isOpen={isOpen} />
+          <Button
+            type="circle"
+            className={classNames(styles.chevronButton, { [styles.opened]: isOpen })}
+          >
+            <ChevronDown />
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BorrowCard
+
+const MarketMainInfo: FC<{ market: MarketPreview }> = ({ market }) => {
+  const { collectionName, collectionImage, collectionFloor, tensorSlug } = market
+
+  return (
+    <div className={styles.mainInfoContainer}>
+      <img src={collectionImage} className={styles.collectionImage} />
+      <div className={styles.mainInfoContent}>
+        <div className={styles.collectionInfo}>
+          <h4 className={styles.collectionName}>{collectionName}</h4>
+          {tensorSlug && <TensorLink className={styles.tensorLink} slug={tensorSlug} />}
+        </div>
+        <StatInfo
+          label="Floor"
+          value={<DisplayValue value={collectionFloor} />}
+          tooltipText="Highest offer among all lenders providing liquidity for this collection"
+        />
+      </div>
+    </div>
+  )
+}
+
+interface MarketAdditionalInfoProps {
+  market: MarketPreview
+  isOpen: boolean
+}
+const MarketAdditionalInfo: FC<MarketAdditionalInfoProps> = ({ market, isOpen }) => {
+  const { loansTvl, activeBondsAmount } = market
+
+  //TODO: ask about apr
+  const MAX_APR = 104
+
+  return (
+    <div className={classNames(styles.additionalInfoStats, { [styles.opened]: isOpen })}>
+      <StatInfo
+        label="In loans"
+        value={<DisplayValue value={loansTvl} />}
+        secondValue={`in ${activeBondsAmount} loans`}
+        tooltipText="Liquidity that is locked in active loans"
+        classNamesProps={{ container: styles.additionalStat }}
+      />
+      <StatInfo
+        label="Apr"
+        value={MAX_APR}
+        tooltipText="Maximum annual interest rate. Ranges between 34-104% APR depending on the loan-to-value (LTV) offered, and becomes fixed once offer is taken"
+        valueType={VALUES_TYPES.PERCENT}
+        classNamesProps={{ container: styles.additionalStat, value: styles.additionalAprStat }}
+      />
+    </div>
+  )
+}
