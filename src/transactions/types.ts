1946794873d3184b675db01827a0e38062fb3873
sablevsky <vladislav1145@gmail.com>
2024-04-26 19:30:02 +0200

    Remove unused TransactionParams<T> type

--- src/transactions/types.ts
+++ src/transactions/types.ts
@@ -1,9 +1,5 @@
 import { SnackbarType } from '@banx/utils'
 
-export type TransactionParams<T> = T extends (arg: infer Arg) => void
-  ? Omit<Arg, 'connection' | 'wallet'>
-  : never
-
 export enum TxnErrorHumanName {
   TRANSACTION_REJECTED = 'Transaction rejected',
   INSUFFICIENT_LAMPORTS = 'Not enough funds',
