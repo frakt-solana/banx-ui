ccceaab24c1d778f80013e1d1171232556e29d23
sablevsky <vladislav1145@gmail.com>
2024-05-28 13:07:27 +0200

    Implement calculateOfferSize util

--- src/utils/core/index.ts
+++ src/utils/core/index.ts
@@ -16,5 +16,22 @@ export const calcWeightedAverage = (nums: number[], weights: number[]) => {
   return weightedAverage || 0
 }
 
+type CalculateOfferSizeParams = {
+  loanValue: number
+  loansAmount: number
+  deltaValue: number
+}
+export const calculateOfferSize = ({
+  loanValue,
+  loansAmount,
+  deltaValue,
+}: CalculateOfferSizeParams): number => {
+  //? Sum of arithmetic progression
+  const a_n = loanValue - deltaValue * (loansAmount - 1)
+  const S = ((loanValue + a_n) * loansAmount) / 2
+
+  return S
+}
+
 export * from './loans'
 export * from './offers'
