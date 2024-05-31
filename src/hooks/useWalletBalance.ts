96f1325e78ef527febc2cfe84f2975b4fe69ef10
sablevsky <vladislav1145@gmail.com>
2024-05-31 12:57:43 +0200

    Remove banxSOL balance in wallet modal

--- src/hooks/useWalletBalance.ts
+++ src/hooks/useWalletBalance.ts
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query'
 import { web3 } from 'fbonds-core'
 import { LendingTokenType } from 'fbonds-core/lib/fbond-protocol/types'
 
-import { BANX_SOL_ADDRESS, BONDS, USDC_ADDRESS } from '@banx/constants'
+import { BONDS, USDC_ADDRESS } from '@banx/constants'
 import { isBanxSolTokenType, isSolTokenType, isUsdcTokenType } from '@banx/utils'
 
 type UseNativeAccount = ({ isLive }: { isLive?: boolean }) => web3.AccountInfo<Buffer> | null
@@ -50,12 +50,6 @@ const useUsdcBalance: UseTokenBalance = (options) => {
   return useTokenBalance(USDC_ADDRESS, { isLive })
 }
 
-const useBanxSolBalance: UseTokenBalance = (options) => {
-  const { isLive = false } = options || {}
-
-  return useTokenBalance(BANX_SOL_ADDRESS, { isLive })
-}
-
 const useTokenBalance = (tokenAddress: string, options?: Options) => {
   const { isLive = false } = options || {}
 
@@ -92,14 +86,9 @@ export const useWalletBalance = (tokenType: LendingTokenType, options?: Options)
 
   const usdcBalance = useUsdcBalance({ isLive })
   const solanaBalance = useSolanaBalance({ isLive })
-  const banxSolBalance = useBanxSolBalance({ isLive })
 
-  if (isSolTokenType(tokenType)) {
+  if (isSolTokenType(tokenType) || isBanxSolTokenType(tokenType)) {
     return solanaBalance
-  }
-
-  if (isBanxSolTokenType(tokenType)) {
-    return banxSolBalance
   }
 
   if (isUsdcTokenType(tokenType)) {
