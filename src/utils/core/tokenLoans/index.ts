8ba7e9e9bd8d18c9974ce8e711a47d8b0a8979bd
iamsphere <ezekiel.9218@gmail.com>
2024-06-05 15:22:23 +0400

    Add token loans utils

--- src/utils/core/tokenLoans/index.ts
+++ src/utils/core/tokenLoans/index.ts
@@ -1,6 +1,10 @@
+import { BN } from 'fbonds-core'
+import { calculateCurrentInterestSolPure } from 'fbonds-core/lib/fbond-protocol/functions/perpetual'
 import { BondTradeTransactionV2State } from 'fbonds-core/lib/fbond-protocol/types'
+import moment from 'moment'
 
 import { core } from '@banx/api/tokens'
+import { SECONDS_IN_72_HOURS } from '@banx/constants'
 
 export const isTokenLoanFrozen = (loan: core.TokenLoan) => {
   return !!loan.bondTradeTransaction.terminationFreeze
@@ -11,4 +15,55 @@ export const isTokenLoanListed = (loan: core.TokenLoan) => {
     loan.bondTradeTransaction.bondTradeTransactionState ===
     BondTradeTransactionV2State.PerpetualBorrowerListing
   )
+}
+
+export const isTokenLoanTerminating = (loan: core.TokenLoan) => {
+  return (
+    loan.bondTradeTransaction.bondTradeTransactionState ===
+    BondTradeTransactionV2State.PerpetualManualTerminating
+  )
+}
+
+export const isTokenLoanLiquidated = (loan: core.TokenLoan) => {
+  if (!loan.fraktBond.refinanceAuctionStartedAt) return false
+
+  const currentTimeInSeconds = moment().unix()
+  const expiredAt = loan.fraktBond.refinanceAuctionStartedAt + SECONDS_IN_72_HOURS
+  return currentTimeInSeconds > expiredAt
+}
+
+export const isTokenLoanActive = (loan: core.TokenLoan) => {
+  const { bondTradeTransactionState } = loan.bondTradeTransaction
+
+  return (
+    bondTradeTransactionState === BondTradeTransactionV2State.PerpetualActive ||
+    bondTradeTransactionState === BondTradeTransactionV2State.PerpetualRefinancedActive
+  )
+}
+
+export const calculateTokenLoanValueWithUpfrontFee = (loan: core.TokenLoan) => {
+  const { solAmount, feeAmount } = loan.bondTradeTransaction
+  return new BN(solAmount).add(new BN(feeAmount))
+}
+
+export const calculateLentTokenValueWithInterest = (loan: core.TokenLoan) => {
+  const loanValueWithUpfrontFee = calculateTokenLoanValueWithUpfrontFee(loan)
+  const accruedInterest = calculateTokenLoanAccruedInterest(loan)
+
+  return loanValueWithUpfrontFee.add(accruedInterest)
+}
+
+export const calculateTokenLoanAccruedInterest = (loan: core.TokenLoan) => {
+  const { amountOfBonds, soldAt } = loan.bondTradeTransaction
+
+  const loanValueWithUpfrontFee = calculateTokenLoanValueWithUpfrontFee(loan)
+
+  const accruedInterest = calculateCurrentInterestSolPure({
+    loanValue: loanValueWithUpfrontFee.toNumber(),
+    startTime: soldAt,
+    currentTime: moment().unix(),
+    rateBasePoints: amountOfBonds,
+  })
+
+  return new BN(accruedInterest)
 }
