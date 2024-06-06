511ff11ba87aa418c53ad033d5ac0b4a5c5e8951
Vladislav <60342317+iamsphere@users.noreply.github.com>
2024-06-06 19:06:29 +0400

    BAN-2365: Implement Borrow swap / List loans UI (#850)
    * Add ModeSwitcher component and useModeType hook
    
    * Add token lending routes and pages
    
    * Implement navigation with mode param in Navbar and BurgerMenu
    
    * Implement navigation for all nfts pages with mode and tokens params
    
    * Modify createPathWithParams, move to functions file
    
    * Add switchers to page header components
    
    * Change styles on TokenSwitcher components
    
    * Rename useTokenType to useNftTokenType
    
    * Small ui changes
    
    * Update OnboardButton component
    
    * Implement TokenDropdown component, refactor TokenSwitcher component
    
    * Add titles to NftTokenSwitcher and TokenSwitcher
    
    * Change PageHeader container
    
    * Add TokenType to useWalletBalance hook
    
    * Change  chevron icon color on dark theme
    
    * Rename token pages to {name}TokenPage
    
    * Rename createPathWithParams to createPathWithModeParams
    
    * Add InstantLendTokenTable component
    
    * Add TokenLoan and TokenMarketPreview types, add request
    
    * Implement PlaceTokenOffers component with mock data
    
    * Add PlaceTokenOfferSection, implement form with mock data
    
    * Add OrderBook component
    
    * Add token offers transactions
    
    * Add Summary component to InstantLendTokenTable
    
    * Add getErrorMessage helper for PlaceTokenOfferSection
    
    * Use useNftTokenType instead of useTokenType, remove useTokenType
    
    * Use real data for fetchTokenMarketsPreview request
    
    * Add useTokenMarketOffers request
    
    * Implement useSyntheticTokenOffers hook
    
    * Add useTokenOffersOptimistic hook
    
    * Refactor token lend page
    
    * Add fetchTokenOffersPreview request and TokenOfferPreview type
    
    * Add getBondingCurveTypeFromLendingToken helper
    
    * Add OffersTokenTabContent components
    
    * Add ExpandedCardContent for OfferTokenCard
    
    * Modify useTokenOffer
    
    * Add LoansStatus component to OfferTokenCard, add CoinPlus icon
    
    * Add collateralsPerToken to update offer transactions, change formatting
    
    * Add collateralIcon to OfferTokenCard
    
    * Change Input step by token type, add todo comments
    
    * Add columns content with mock data for InstantLendTokenTable
    
    * Add auction and freeze filters button, search select for InstantLendTokenTable
    
    * Add TODO comments
    
    * Add InstantBorrowContent components with mock data
    
    * Implement InputTokenSelect component with mock data
    
    * Add ListLoansContent components, hooks. Refactor files structure
    
    * Add ControlsButtons to InputTokenSelect
    
    * Remove helpers file from api/tokens
    
    * Update BorrowTokenPage UI
    
    * Add label prop to LoanValueSlider

--- src/pages/tokenLending/BorrowTokenPage/BorrowTokenPage.tsx
+++ src/pages/tokenLending/BorrowTokenPage/BorrowTokenPage.tsx
@@ -1,3 +1,58 @@
+import { useEffect } from 'react'
+
+import { Tab, Tabs, useTabs } from '@banx/components/Tabs'
+
+import BorrowHeader from './BorrowHeader'
+import InstantBorrowContent from './InstantBorrowContent'
+import ListLoansContent from './ListLoansContent'
+import { useBorrowTokenTabs } from './hooks'
+
+import styles from './BorrowTokenPage.module.less'
+
 export const BorrowTokenPage = () => {
-  return <div>BorrowTokenPage</div>
+  //? Used to set default tab when user is redirected to BorrowTokenPage.
+  const { tab: storeTab, setTab } = useBorrowTokenTabs()
+
+  const {
+    value: currentTabValue,
+    setValue,
+    tabs,
+  } = useTabs({
+    tabs: BORROW_TABS,
+    defaultValue: storeTab ?? BorrowTokenTabName.INSTANT,
+  })
+
+  //? Used hook to reset store when the component is unmounted
+  useEffect(() => {
+    if (!storeTab) return
+
+    return () => setTab(null)
+  }, [setTab, storeTab])
+
+  return (
+    <>
+      <BorrowHeader />
+      <Tabs value={currentTabValue} tabs={tabs} setValue={setValue} />
+      <div className={styles.content}>
+        {currentTabValue === BorrowTokenTabName.INSTANT && <InstantBorrowContent />}
+        {currentTabValue === BorrowTokenTabName.LIST && <ListLoansContent />}
+      </div>
+    </>
+  )
 }
+
+export enum BorrowTokenTabName {
+  INSTANT = 'instant',
+  LIST = 'list',
+}
+
+const BORROW_TABS: Tab[] = [
+  {
+    label: 'Borrow now',
+    value: BorrowTokenTabName.INSTANT,
+  },
+  {
+    label: 'List loans',
+    value: BorrowTokenTabName.LIST,
+  },
+]
