21b91a9aa8ea2b44cec24170bee4aeb7f59f5765
iamsphere <ezekiel.9218@gmail.com>
2024-07-01 21:19:01 +0400

    Add sort by LTV, Status to ActiveLoansTable

--- src/pages/tokenLending/OffersTokenPage/components/ActiveLoansTable/hooks/useSortedLoans.ts
+++ src/pages/tokenLending/OffersTokenPage/components/ActiveLoansTable/hooks/useSortedLoans.ts
@@ -1,26 +1,59 @@
 import { useMemo, useState } from 'react'
 
-import { orderBy } from 'lodash'
+import { chain, orderBy } from 'lodash'
 
-import { SortOption } from '@banx/components/SortDropdown'
+import { SortOption, SortOrder } from '@banx/components/SortDropdown'
 
 import { core } from '@banx/api/tokens'
+import {
+  caclulateBorrowTokenLoanValue,
+  calculateTokenLoanLtvByLoanValue,
+  isTokenLoanLiquidated,
+  isTokenLoanTerminating,
+} from '@banx/utils'
 
 enum SortField {
+  LTV = 'ltv',
   LENT = 'lent',
   APR = 'apr',
+  STATUS = 'status',
 }
 
 type SortValueGetter = (loan: core.TokenLoan) => number
 
 const SORT_OPTIONS: SortOption<SortField>[] = [
+  { label: 'LTV', value: [SortField.LTV, 'desc'] },
   { label: 'Lent', value: [SortField.LENT, 'desc'] },
   { label: 'APR', value: [SortField.APR, 'desc'] },
+  { label: 'Status', value: [SortField.STATUS, 'desc'] },
 ]
 
 const SORT_VALUE_MAP: Record<SortField, string | SortValueGetter> = {
+  [SortField.LTV]: (loan: core.TokenLoan) => {
+    const debtValue = caclulateBorrowTokenLoanValue(loan).toNumber()
+    return calculateTokenLoanLtvByLoanValue(loan, debtValue)
+  },
   [SortField.LENT]: (loan: core.TokenLoan) => loan.fraktBond.currentPerpetualBorrowed,
   [SortField.APR]: (loan: core.TokenLoan) => loan.bondTradeTransaction.amountOfBonds,
+  [SortField.STATUS]: '',
+}
+
+const sortLoansByStatus = (loans: core.TokenLoan[], order: SortOrder) => {
+  const terminatingLoans = chain(loans)
+    .filter(isTokenLoanTerminating)
+    .sortBy((loan) => loan.fraktBond.refinanceAuctionStartedAt)
+    .reverse()
+    .value()
+
+  const otherLoans = chain(loans)
+    .filter((loan) => !isTokenLoanTerminating(loan) && !isTokenLoanLiquidated(loan))
+    .sortBy((loan) => loan.fraktBond.activatedAt)
+    .reverse()
+    .value()
+
+  const combinedLoans = [...otherLoans, ...terminatingLoans]
+
+  return order === 'asc' ? combinedLoans : combinedLoans.reverse()
 }
 
 export const useSortedLoans = (loans: core.TokenLoan[]) => {
@@ -33,7 +66,9 @@ export const useSortedLoans = (loans: core.TokenLoan[]) => {
 
     const sortValueGetter = SORT_VALUE_MAP[field]
 
-    return orderBy(loans, sortValueGetter, order)
+    return field === SortField.STATUS
+      ? sortLoansByStatus(loans, order)
+      : orderBy(loans, sortValueGetter, order)
   }, [sortOption, loans])
 
   const onChangeSortOption = (option: SortOption<SortField>) => {
