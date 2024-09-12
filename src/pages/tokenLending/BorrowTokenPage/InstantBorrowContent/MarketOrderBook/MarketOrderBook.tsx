43d9d7d0005a59e7c83ba2226bf3f97b2fbb685e
iamsphere <ezekiel.9218@gmail.com>
2024-09-12 16:30:30 +0400

    Filter out offers with ltv > 100

--- src/pages/tokenLending/BorrowTokenPage/InstantBorrowContent/MarketOrderBook/MarketOrderBook.tsx
+++ src/pages/tokenLending/BorrowTokenPage/InstantBorrowContent/MarketOrderBook/MarketOrderBook.tsx
@@ -4,11 +4,17 @@ import { useWallet } from '@solana/wallet-adapter-react'
 import classNames from 'classnames'
 import { chain } from 'lodash'
 
+import { calculateLtvPercent } from '@banx/components/PlaceTokenOfferSection'
 import Table from '@banx/components/Table'
 
 import { CollateralToken } from '@banx/api/tokens'
 import { useTokenMarketOffers } from '@banx/pages/tokenLending/LendTokenPage'
 import { useNftTokenType } from '@banx/store/nft'
+import {
+  calculateTokensPerCollateral,
+  formatTokensPerCollateralToStr,
+  getTokenDecimals,
+} from '@banx/utils'
 
 import { getTableColumns } from './columns'
 
@@ -18,6 +24,8 @@ interface MarketOrderBookProps {
   collateral: CollateralToken
 }
 
+const MAX_LTV_THRESHOLD = 100
+
 const MarketOrderBook: FC<MarketOrderBookProps> = ({ collateral }) => {
   const { publicKey } = useWallet()
   const { offers, isLoading } = useTokenMarketOffers(collateral.marketPubkey)
@@ -25,12 +33,30 @@ const MarketOrderBook: FC<MarketOrderBookProps> = ({ collateral }) => {
   const { tokenType } = useNftTokenType()
   const columns = getTableColumns({ collateral, tokenType })
 
+  const marketTokenDecimals = Math.log10(getTokenDecimals(tokenType)) //? 1e9 => 9, 1e6 => 6
+
   const filteredOffers = useMemo(() => {
     return chain(offers)
       .filter((offer) => offer.assetReceiver.toBase58() !== publicKey?.toBase58())
+      .filter((offer) => {
+        const tokensPerCollateral = formatTokensPerCollateralToStr(
+          calculateTokensPerCollateral(
+            offer.validation.collateralsPerToken,
+            collateral.collateral.decimals,
+          ),
+        )
+
+        const ltvPercent = calculateLtvPercent({
+          collateralPerToken: tokensPerCollateral,
+          collateralPrice: collateral.collateralPrice,
+          marketTokenDecimals,
+        })
+
+        return ltvPercent <= MAX_LTV_THRESHOLD
+      })
       .sortBy((offer) => offer.validation.collateralsPerToken.toNumber())
       .value()
-  }, [offers, publicKey])
+  }, [collateral, marketTokenDecimals, offers, publicKey])
 
   return (
     <Table
