8953ce7f20ca5a4700b43a09b5f9690745a24534
sablevsky <vladislav1145@gmail.com>
2024-10-08 09:33:14 +0200

    Update /vault request

--- src/api/shared/requests.ts
+++ src/api/shared/requests.ts
@@ -9,9 +9,9 @@ import { parseResponseSafe } from './validation'
 type FetchUserVaults = (props: { walletPublicKey: string }) => Promise<UserVault[] | undefined>
 
 export const fetchUserVaults: FetchUserVaults = async ({ walletPublicKey }) => {
-  const { data } = await axios.get<{ data: unknown }>(
+  const { data } = await axios.get<{ data: { vaults: unknown } }>(
     `${BACKEND_BASE_URL}/vault/${walletPublicKey}`,
   )
 
-  return await parseResponseSafe<UserVault[]>(data?.data, UserVaultSchema.array())
+  return await parseResponseSafe<UserVault[]>(data?.data?.vaults, UserVaultSchema.array())
 }
