afa16583d7d5b3fb7336adb2ccbf02a8eae8f8f3
iamsphere <ezekiel.9218@gmail.com>
2024-10-10 17:41:02 +0400

    Remove balance content from

--- src/components/WalletModal/components.tsx
+++ src/components/WalletModal/components.tsx
@@ -2,20 +2,13 @@ import { FC } from 'react'
 
 import { useWallet } from '@solana/wallet-adapter-react'
 import classNames from 'classnames'
-import { LendingTokenType } from 'fbonds-core/lib/fbond-protocol/types'
 
-import { useBanxSolBalance, useDiscordUser, useSolanaBalance } from '@banx/hooks'
-import { BanxSOL, ChangeWallet, Copy, SignOut } from '@banx/icons'
-import { useIsLedger, useTokenType } from '@banx/store/common'
-import {
-  copyToClipboard,
-  formatValueByTokenType,
-  isBanxSolTokenType,
-  shortenAddress,
-} from '@banx/utils'
+import { useDiscordUser } from '@banx/hooks'
+import { ChangeWallet, Copy, SignOut } from '@banx/icons'
+import { useIsLedger } from '@banx/store/common'
+import { copyToClipboard, shortenAddress } from '@banx/utils'
 
 import Checkbox from '../Checkbox'
-import { StatInfo } from '../StatInfo'
 import { DisplayValue } from '../TableComponents'
 import UserAvatar from '../UserAvatar'
 import { ClaimSection, EscrowVault } from './LenderVaults'
@@ -28,11 +21,7 @@ const UserGeneralInfo = () => {
   const publicKeyString = publicKey?.toBase58() || ''
   const { data: discordUserData } = useDiscordUser()
 
-  const solWalletBalance = useSolanaBalance({ isLive: true })
-  const banxSolWalletBalance = useBanxSolBalance({ isLive: true })
-
   const { isLedger, setIsLedger } = useIsLedger()
-  const { tokenType } = useTokenType()
 
   return (
     <div className={styles.userGeneralInfoContainer}>
@@ -44,48 +33,6 @@ const UserGeneralInfo = () => {
         </div>
         <Checkbox onChange={() => setIsLedger(!isLedger)} label="I use ledger" checked={isLedger} />
       </div>
-      {isBanxSolTokenType(tokenType) && (
-        <BalanceContent
-          solWalletBalance={solWalletBalance}
-          banxSolWalletBalance={banxSolWalletBalance}
-          tokenType={tokenType}
-        />
-      )}
-    </div>
-  )
-}
-
-interface BalanceContentProps {
-  solWalletBalance: number
-  banxSolWalletBalance: number
-  tokenType: LendingTokenType
-}
-const BalanceContent: FC<BalanceContentProps> = ({
-  solWalletBalance,
-  banxSolWalletBalance,
-  tokenType,
-}) => {
-  const formattedBanxSolWalletBalance = banxSolWalletBalance
-    ? formatValueByTokenType(banxSolWalletBalance, tokenType)
-    : 0
-
-  return (
-    <div className={styles.balanceContainer}>
-      <div className={styles.balanceContent}>
-        <StatInfo
-          value={formattedBanxSolWalletBalance}
-          classNamesProps={{ value: styles.balanceValue }}
-          icon={BanxSOL}
-          flexType="row"
-        />
-        <div className={styles.verticalLine} />
-        <StatInfo
-          value={<DisplayValue value={solWalletBalance} />}
-          classNamesProps={{ value: styles.balanceValue }}
-          flexType="row"
-        />
-      </div>
-      <span className={styles.balanceLabel}>Wallet balance</span>
     </div>
   )
 }
