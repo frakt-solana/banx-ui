a4e3dbff9dbff0ebef492e196d2992d1f25b3e12
iamsphere <ezekiel.9218@gmail.com>
2024-10-16 20:29:57 +0400

    Use lender apr rate in calcTokenWeeklyInterest

--- src/pages/tokenLending/LendTokenPage/InstantLendTokenTable/helpers.ts
+++ src/pages/tokenLending/LendTokenPage/InstantLendTokenTable/helpers.ts
@@ -1,6 +1,4 @@
-import { web3 } from 'fbonds-core'
 import { calculateCurrentInterestSolPure } from 'fbonds-core/lib/fbond-protocol/functions/perpetual'
-import { calcBorrowerTokenAPR } from 'fbonds-core/lib/fbond-protocol/helpers'
 
 import { core } from '@banx/api/tokens'
 import { SECONDS_IN_DAY } from '@banx/constants'
@@ -23,9 +21,6 @@ export const calcTokenWeeklyInterest = (loan: core.TokenLoan) => {
     loanValue: calculateLendToBorrowValue(loan),
     startTime: soldAt,
     currentTime: soldAt + SECONDS_IN_DAY * 7,
-    rateBasePoints: calcBorrowerTokenAPR(
-      amountOfBonds,
-      new web3.PublicKey(loan.fraktBond.hadoMarket),
-    ),
+    rateBasePoints: amountOfBonds,
   })
 }
