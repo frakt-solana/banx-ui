5b1990cff39dfafd5286475ad9251230cdcbc180
Vladislav <60342317+iamsphere@users.noreply.github.com>
2024-10-17 10:26:48 +0400

    BAN-2747: Fix weekly interest on token lend now (#1017)
    * Fix wrong weekly interest calculation on InstantLendTokenTable
    
    * Use lender apr rate in calcTokenWeeklyInterest

--- src/pages/tokenLending/LendTokenPage/InstantLendTokenTable/helpers.ts
+++ src/pages/tokenLending/LendTokenPage/InstantLendTokenTable/helpers.ts
@@ -1,6 +1,4 @@
-import { web3 } from 'fbonds-core'
 import { calculateCurrentInterestSolPure } from 'fbonds-core/lib/fbond-protocol/functions/perpetual'
-import { calcBorrowerTokenAPR } from 'fbonds-core/lib/fbond-protocol/helpers'
 
 import { core } from '@banx/api/tokens'
 import { SECONDS_IN_DAY } from '@banx/constants'
@@ -20,12 +18,9 @@ export const calcTokenWeeklyInterest = (loan: core.TokenLoan) => {
   const { soldAt, amountOfBonds } = loan.bondTradeTransaction
 
   return calculateCurrentInterestSolPure({
-    loanValue: loan.bondTradeTransaction.amountOfBonds,
+    loanValue: calculateLendToBorrowValue(loan),
     startTime: soldAt,
     currentTime: soldAt + SECONDS_IN_DAY * 7,
-    rateBasePoints: calcBorrowerTokenAPR(
-      amountOfBonds,
-      new web3.PublicKey(loan.fraktBond.hadoMarket),
-    ),
+    rateBasePoints: amountOfBonds,
   })
 }
